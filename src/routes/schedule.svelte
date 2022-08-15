0aff4f83f4e79852cbee2af7cdbe18df34a3c9ee
Kattenelvis <officialkattenelvis@gmail.com>
2022-08-15 16:11:13 +0200

    calendar and images work

---
+++ src/routes/schedule.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+import Header from "$lib/Header/Header.svelte";
+
+	const months = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec'
+	];
+
+	const currentDate = new Date();
+	let month = currentDate.getMonth();
+	let year = currentDate.getFullYear();
+	let selectedDay = currentDate.getDate();
+	let polls = [];
+	let loading = false;
+	let pollList = [];
+</script>
+<Header />
+
+<div on:click={() => year -= 1}>{"<"}</div>
+{year}
+<div on:click={() => year += 1}>{">"}</div>
+
+<div on:click={() => month -= 1}>{"<"}</div>
+{months[month]}
+<div on:click={() => month += 1}>{">"}</div>
+
+

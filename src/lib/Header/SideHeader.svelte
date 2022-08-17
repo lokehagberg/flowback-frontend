11baf5e532050675fd1cf49239e6372e472783a3
Kattenelvis <officialkattenelvis@gmail.com>
2022-08-17 19:46:17 +0200

    Added Tools

--- src/lib/Header/SideHeader.svelte
+++ src/lib/Header/SideHeader.svelte
@@ -8,17 +8,20 @@
 		window.location.href = '/';
 	};
 
+	const handleCoDocumentCreation = () => {};
+
 	const navs = [
 		{
 			title: 'User profile',
 			action: () => (window.location.href = '/user')
 		},
-		{ title: 'Support', action: () => (open = true) },
-		{ title: 'Tools' },
+		{ title: 'Support', action: () => (open_support = true) },
+		{ title: 'Tools', action: () => (open_tools = true) },
 		{ title: 'Logout', action: logOut }
 	];
 
-	let open = true;
+	let open_support = false;
+	let open_tools = false;
 </script>
 
 <div class="absolute right-0 bg-gray-500">
@@ -27,10 +30,10 @@
 	{/each}
 </div>
 
-<Modal {open}>
+<Modal open={open_support}>
 	<div slot="header" class="p-4">
 		<h1 class="text-sm text-left p-0">Support</h1>
-		<CrossButton action={() => (open = false)} />
+		<CrossButton action={() => (open_support = false)} />
 	</div>
 
 	<div slot="body" class="text-left">
@@ -38,5 +41,24 @@
 		<div>Number: +46737482562</div>
 		<div>Mail: flowbacktask@gmail.com</div>
 		<div>For questions about Metamask, contact the Metamask team.</div>
+	</div>
+</Modal>
+
+<Modal open={open_tools}>
+	<div slot="header" class="p-4">
+		<h1 class="text-sm text-left p-0">Tools</h1>
+		<CrossButton action={() => (open_tools = false)} />
+	</div>
+
+	<div slot="body" class="text-left">
+		<div class="py-3 mb-2">
+			Click this button to create a co-document, share the link for others to edit it. Remember to
+			save the link.
+		</div>
+		<div class="grupper-card row g-2 clickable">
+			<div class="text-center my-2 noSelect">
+				<div on:click={handleCoDocumentCreation}>+ Create Co-Document</div>
+			</div>
+		</div>
 	</div>
 </Modal>

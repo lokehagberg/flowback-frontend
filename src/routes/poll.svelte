36ec7e1ace231b198d854a32632cc4b6a72cd908
Kattenelvis <officialkattenelvis@gmail.com>
2022-08-22 21:49:50 +0200

    poll

--- src/routes/poll.svelte
+++ src/routes/poll.svelte
@@ -1,47 +1,80 @@
-<script>
+<script lang="ts">
 	import Proposal from '../lib/Poll/Proposal.svelte';
 	import Header from '$lib/Header/Header.svelte';
 
-    let abstained = [
-        {title:"Nuclear Option", description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
-			earum, nemo error necessitatibus minima eligendi voluptatem vel? Praesentium dicta reiciendis
-			ut modi id iure minus amet.`},
-        {title:"Geothermal Power explosion", description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
-			earum, nemo error necessitatibus minima eligendi voluptatem vel? Praesentium dicta reiciendis
-			ut modi id iure minus amet.`},
-        {title:"Spread evil pollen", description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
-			earum, nemo error necessitatibus minima eligendi voluptatem vel? Praesentium dicta reiciendis
-			ut modi id iure minus amet.`},
-    ]
+	interface Proposal {
+		title:string,
+		description:string,
+		id:number
+	}
 
-    let ranked = [
-        {title:"Nuclear Option", description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
+	let abstained: Proposal[] = [
+		{
+			title: 'Nuclear Option',
+			description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
 			earum, nemo error necessitatibus minima eligendi voluptatem vel? Praesentium dicta reiciendis
-			ut modi id iure minus amet.`},
-        {title:"Geothermal Power explosion", description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
+			ut modi id iure minus amet.`,
+			id:1
+		},
+		{
+			title: 'Geothermal Power explosion',
+			description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
 			earum, nemo error necessitatibus minima eligendi voluptatem vel? Praesentium dicta reiciendis
-			ut modi id iure minus amet.`},
-        {title:"Spread evil pollen", description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
+			ut modi id iure minus amet.`,
+			id:2
+		},
+		{
+			title: 'Evil Pollen',
+			description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia repudiandae nesciunt ipsam quo
 			earum, nemo error necessitatibus minima eligendi voluptatem vel? Praesentium dicta reiciendis
-			ut modi id iure minus amet.`},
-    ]
+			ut modi id iure minus amet.`,
+			id:3
+		}
+	];
+
+	let ranked: Proposal[] = [
+
+	];
+
+	const moveProposal = (proposalId1:number, proposalId2:number) => {
+
+
+
+	}
+	const handleRankChange = (proposal1:Proposal, proposal2:Proposal) => {
+
+	}
 </script>
 
 <Header />
-<h1>Destroy Lithuania</h1>
-
-<div class="flex gap-8 p-10 m-10 bg-gray-200">
-	<div class="flex flex-col gap-6 w-1/2">
-		<div>Abstain</div>
-        {#each abstained as proposal}
-            <Proposal title={proposal.title} description={proposal.description} />
-        {/each}
-	</div>
-	<div class="flex flex-col gap-6 w-1/2">
-		<div>Rank</div>
-        {#each ranked as proposal}
-            <Proposal title={proposal.title} description={proposal.description} />
-        {/each}
-
+<div class="p-10 m-10 bg-gray-200">
+	<h1>Destroy Lithuania</h1>
+	<div class="poll border border-gray-700">
+		<div style="grid-column: 1 / 2; grid-row: 1 / 2">Abstain</div>
+		<div style="grid-column: 2 / 3; grid-row: 1 / 2">Rank</div>
+		{#each abstained as proposal, i}
+			<div style={`grid-column: 1 / 2; grid-row:${i + 2} / ${i + 2 + 1}`}>
+				<Proposal title={proposal.title} description={proposal.description} />
+			</div>
+		{/each}
+		{#each ranked as proposal, i}
+			<div style={`grid-column: 2 / 3; grid-row:${i + 2} / ${i + 2 + 1}`}>
+				<Proposal title={proposal.title} description={proposal.description} />
+			</div>
+		{/each}
+		{#each ranked as num, i}
+			<div style={`grid-column: 3 / 4; grid-row: ${i + 2} / ${i + 2 + 1}; text-align:center`}>
+				{i + 1}
+			</div>
+		{/each}
 	</div>
 </div>
+
+<style>
+	.poll {
+		display: grid;
+		grid-template-columns: 8fr 8fr 1fr;
+
+		align-items: center;
+	}
+</style>

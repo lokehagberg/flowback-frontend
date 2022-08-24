306799c19bd7094319b65c5d008cb5080a232a58
Kattenelvis <officialkattenelvis@gmail.com>
2022-08-24 22:03:24 +0200

    Added comments

---
+++ src/lib/Poll/Comments.svelte
@@ -0,0 +1,18 @@
+<script>
+import ButtonPrimary from "$lib/Generic/ButtonPrimary.svelte";
+import TextInput from "$lib/Generic/TextInput.svelte";
+
+</script>
+<div>
+    Comments
+    <form>
+        <TextInput  label="Comment" />
+        <ButtonPrimary>Submit</ButtonPrimary>
+    </form>
+
+    <div>
+        <div>Hi</div>
+        <div>Racial Sluts</div>
+        <div>Racial Slurs</div>
+    </div>
+</div>
--- src/routes/poll.svelte
+++ src/routes/poll.svelte
@@ -2,6 +2,7 @@
 	import ProposalsRanked from '$lib/Poll/ProposalsRanked.svelte';
 	import Header from '$lib/Header/Header.svelte';
 	import Timeline from '$lib/Poll/Timeline.svelte';
+	import Comments from '$lib/Poll/Comments.svelte';
 </script>
 
 <Header />
@@ -9,4 +10,5 @@
 	<h1>Destroy Lithuania</h1>
 	<ProposalsRanked />
 	<Timeline />
+	<Comments />
 </div>

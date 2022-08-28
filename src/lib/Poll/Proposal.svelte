a56d16f0aef4073c772d60ccd1208c580a687e16
Kattenelvis <officialkattenelvis@gmail.com>
2022-08-28 12:46:21 +0200

    p

--- src/lib/Poll/Proposal.svelte
+++ src/lib/Poll/Proposal.svelte
@@ -2,11 +2,25 @@
 	import Fa from 'svelte-fa/src/fa.svelte';
 	import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 
-	export let title:string
-	export let description:string
+	export let title: string;
+	export let description: string;
+
+	let isHoveredOver = false;
 </script>
 
-<div class="flex items-center bg-white gap-8 p-4 border border-gray-200 lg:h-60 xl:h-40">
+<div
+	on:dragenter|preventDefault={() => (isHoveredOver = false)}
+	on:dragleave|preventDefault={() => (isHoveredOver = true)}
+	class:hidden={!isHoveredOver}
+	class="h-60 xl:h-40 p-4 bg-gray-200"
+/>
+<div
+	draggable="true"
+	class="proposal cursor-move flex items-center bg-white gap-8 p-4 border border-gray-200 lg:h-60 xl:h-40"
+	on:dragenter|preventDefault={() => (isHoveredOver = true)}
+	on:dragleave|preventDefault={() => (isHoveredOver = false)}
+	class:hidden={isHoveredOver}
+>
 	<div><Fa icon={faBars} /></div>
 	<div class="h-full">
 		<h1 class="text-lg text-left">{title}</h1>
@@ -14,8 +28,8 @@
 			{description}
 		</p>
 	</div>
-	<slot name="right-icons">
-
-	</slot>
+	<slot name="right-icons" />
 </div>
 
+<style>
+</style>

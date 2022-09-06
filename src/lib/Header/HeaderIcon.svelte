a97528ee726693d4a7a787d3e083a4f794ee3996
Kattenelvis <officialkattenelvis@gmail.com>
2022-09-06 16:22:54 +0200

    Timeline work

--- src/lib/Header/HeaderIcon.svelte
+++ src/lib/Header/HeaderIcon.svelte
@@ -1,28 +1,34 @@
 <script lang="ts">
 	import Fa from 'svelte-fa/src/fa.svelte';
 	import { faCircle } from '@fortawesome/free-solid-svg-icons/faCircle';
-	// import { onMount } from 'svelte/types/runtime/internal/lifecycle';
 	export let icon = faCircle;
 	export let text = 'icon';
 	export let href = '/';
+	export let className = '';
+	export let color = '';
+	export let size = 'xl';
 
 	let hovering = false;
 	let selectedPage = false;
-	
+
 	function checkIfSelected() {
 		selectedPage = window.location.pathname === '/' + href;
-	};
+	}
 </script>
 
 <a
 	on:mouseover={() => (hovering = true)}
 	on:mouseleave={() => (hovering = false)}
 	on:focus
-	href={"/" + href}
-	class="p-5 relative cursor-pointer"
+	href={'/' + href}
+	class={`p-5 relative cursor-pointer ${className}`}
 >
 	<div on:load={checkIfSelected}>
-		<Fa {icon} color={hovering ? '#015BC0' : selectedPage ? 'lightgray' : 'black'} />
+		<Fa
+			{icon}
+			{size}
+			color={color !== '' ? color : hovering ? '#015BC0' : selectedPage ? 'lightgray' : 'black'} 
+		/>
 	</div>
 	<div
 		class="text-black p-1 bg-white mt-4 border border-gray-400 rounded text-sm header-icon z-50"
--- src/lib/Poll/Timeline.svelte
+++ src/lib/Poll/Timeline.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import HeaderIcon from '$lib/Header/HeaderIcon.svelte';
+	import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
 
-	let dates = [new Date('2022-08-24'), new Date('2022-08-25'), new Date('2022-09-03')];
+	let dates = [new Date('2022-08-26'), new Date('2022-09-14'), new Date('2022-09-15'), new Date('2022-09-25')];
 	let date = new Date();
-
+	const dateLabels = ["Start", "Proposal end", "Delegate end", "End"]
 	const totalTime = dates[dates.length - 1].getTime() - dates[0].getTime();
 
 	const toNowTime = date.getTime() - dates[0].getTime();
@@ -21,15 +22,25 @@
 	<div class="h-6">
 		{#each datePlacement as date, i}
 			<div class="absolute" style:left={`calc(${date}% - 0.75rem)`}>
-				<HeaderIcon text={`${dates[i]}`} />
+				<HeaderIcon size="1.5x" text={`${dates[i]}`} icon={faExclamationCircle} color={`${dates[i] <= new Date() ? "#015BC0" : ""}`}/>
 			</div>
 		{/each}
 	</div>
-	<div class="h-10 mt-2">
+	<div class="h-10 mt-10">
 		<div class={`absolute bg-gray-300 left-0 h-6 rounded-full w-full`} />
 		<div
 			class={`absolute bg-blue-400 left-0 h-6 rounded-full`}
 			style:width={`${progress < 100 ? progress : 100}%`}
 		/>
+	</div>
+	<div class="border border-gray-200 p-6">
+		<ul>
+			{#each dateLabels as label, i}
+				<li class="flex justify-between">
+					<div>{label} date:</div> 
+					<div>{dates[i]}</div>
+				</li>
+			{/each}
+		</ul>
 	</div>
 </div>

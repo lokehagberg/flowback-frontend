3cc60fb14cb3bff7ccfe6856a3322407bff31c24
Kattenelvis <officialkattenelvis@gmail.com>
2022-09-07 15:22:11 +0200

    fixing text area

--- src/lib/Generic/TextArea.svelte
+++ src/lib/Generic/TextArea.svelte
@@ -2,7 +2,7 @@
 	export let value = '';
 	export let label: string;
 
-	const f = (e:any) => {
+	const expandTextArea = (e:any) => {
 		console.log(e.dataset)
 		e.parentNode.dataset.replicatedValue = e.value
 	}
@@ -11,7 +11,7 @@
 <label class=""
 	><p class="text-sm mb-1">{label}</p>
 	<textarea
-		on:input={f}
+		on:input={expandTextArea}
 		bind:value
 		class="border border-gray-300 border-solid rounded focus:bg-gray-100 p-0.5 w-full outline-none"
 	/>

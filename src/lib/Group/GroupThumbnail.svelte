6b8c93fb851e1e265d03bfb0c3e750827fc47b7c
Kattenelvis <officialkattenelvis@gmail.com>
2022-09-20 00:06:15 +0200

    Mobile phoning

--- src/lib/Group/GroupThumbnail.svelte
+++ src/lib/Group/GroupThumbnail.svelte
@@ -6,14 +6,18 @@
 	export let group: Group;
 
 	const goToGroup = () => {
-		window.location.href = `/groups/${group.id}`
-	}
+		window.location.href = `/groups/${group.id}`;
+	};
 </script>
 
-<div class="w-1/2 bg-white relative shadow-md hover:outline vote-thumbnail cursor-pointer">
+<div class="w-5/6 md:w-1/2 bg-white relative shadow-md hover:outline vote-thumbnail cursor-pointer">
 	<div on:click={goToGroup}>
-		<img src={`${import.meta.env.VITE_API}${group.cover_image}`} class="cover" alt="cover"/>
-		<img src={`${import.meta.env.VITE_API}${group.image}`} class="absolute top-6 left-6 profile" alt="profile"/>
+		<img src={`${import.meta.env.VITE_API}${group.cover_image}`} class="cover" alt="cover" />
+		<img
+			src={`${import.meta.env.VITE_API}${group.image}`}
+			class="absolute top-6 left-6 profile"
+			alt="profile"
+		/>
 	</div>
 	<div class="flex flex-col gap-6">
 		<h1 class="text-2xl p-4 pl-6 text-left" on:click={goToGroup}>
@@ -24,8 +28,12 @@
 		</p>
 		<ButtonPrimary
 			action={async () => {
-				const res = await fetchRequest('POST', `group/${group.id}/${group.joined ? "leave" : "join"}`, {})
-				if (res.ok) group.joined = !group.joined
+				const { res } = await fetchRequest(
+					'POST',
+					`group/${group.id}/${group.joined ? 'leave' : 'join'}`,
+					{}
+				);
+				if (res.ok) group.joined = !group.joined;
 			}}
 			className="hover:bg-blue-800 2xl:pt-6 2xl:pb-6 "
 			>{group.joined ? 'Leave' : 'Join'}</ButtonPrimary
@@ -33,19 +41,18 @@
 	</div>
 </div>
 
-
 <style>
 	.vote-thumbnail:hover {
 		outline: 2px #ccc solid;
 	}
 
-	img.cover{
+	img.cover {
 		aspect-ratio: 4;
-		width:100%;
+		width: 100%;
 	}
 
-	img.profile{
-		width:100px;
-		height:100px;
+	img.profile {
+		width: 100px;
+		height: 100px;
 	}
 </style>

251234e83889c3665f11a7537b69f23bcf566f55
Kattenelvis <officialkattenelvis@gmail.com>
2022-10-12 21:15:21 +0200

    Sidebar fixed for mobile

--- src/lib/Group/GroupSidebar.svelte
+++ src/lib/Group/GroupSidebar.svelte
@@ -8,62 +8,83 @@
 	import { faCircleInfo } from '@fortawesome/free-solid-svg-icons/faCircleInfo';
 	import { faVideoCamera } from '@fortawesome/free-solid-svg-icons/faVideoCamera';
 	import { faMailReplyAll } from '@fortawesome/free-solid-svg-icons/faMailReplyAll';
+	import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+	import { faX } from '@fortawesome/free-solid-svg-icons/faX';
 	import { faCog } from '@fortawesome/free-solid-svg-icons/faCog';
 	import { faPoll } from '@fortawesome/free-solid-svg-icons/faPoll';
 	import { page } from '$app/stores';
+	import Fa from 'svelte-fa/src/fa.svelte';
 
 	export let selectedPage: SelectablePage = 'flow';
 	export let group: GroupDetails;
+
+	let innerWidth = 0;
+	let clickedExpandSidebar = false;
+
+	$: console.log(innerWidth);
 </script>
 
+<svelte:window bind:innerWidth />
+
 <div>
-	<div class="bg-white shadow rounded flex flex-col">
-		<GroupSidebarButton action={() => (selectedPage = 'flow')} text="Flow" />
-		<GroupSidebarButton
-			action={() => (selectedPage = 'delegation')}
-			text="Delegation"
-			icon={faPeopleArrows}
-		/>
-		<GroupSidebarButton
-			action={() => (selectedPage = 'documents')}
-			text="Documents"
-			icon={faFile}
-		/>
-		<GroupSidebarButton
-			action={() => (selectedPage = 'members')}
-			text="Members"
-			icon={faUserGroup}
-		/>
-		<GroupSidebarButton
-			action={() => (selectedPage = 'statistics')}
-			text="Statistics"
-			icon={faChartColumn}
-		/>
-		<GroupSidebarButton action={() => (selectedPage = 'about')} text="About" icon={faCircleInfo} />
-	</div>
-	<div class="bg-white shadow rounded flex flex-col mt-6">
-		<GroupSidebarButton
-			action={() => (window.location.href = `/createpoll?id=${$page.params.groupId}`)}
-			text="Create Poll"
-			icon={faPoll}
-		/>
-		<GroupSidebarButton
-			action={() => (window.location.href = `https://meet.flowback.org/${group.jitsi_room}`)}
-			text="Video Conference"
-			icon={faVideoCamera}
-		/>
-	</div>
-	<div class="bg-white shadow rounded flex flex-col mt-6">
-		<GroupSidebarButton
-			action={() => (selectedPage = 'email')}
-			text="Send Email"
-			icon={faMailReplyAll}
-		/>
-		<GroupSidebarButton action={() => (selectedPage = 'tags')} text="Edit Tags" icon={faCog} />
-		<GroupSidebarButton
-			action={() => (window.location.href = `/creategroup?group=${$page.params.groupId}`)}
-			text="Edit Group"
-			icon={faCog}
-		/>
-	</div>
+	{#if innerWidth < 700 && !clickedExpandSidebar}
+		<div on:click={() => (clickedExpandSidebar = true)} class="bg-white p-6 cursor-pointer absolute shadow rounded"><Fa icon={faBars} /></div>
+	{:else}
+		{#if innerWidth < 700}
+			<div on:click={() => (clickedExpandSidebar = false)} class="bg-white p-6 cursor-pointer shadow rounded flex justify-around items-center"><Fa icon={faX} /><span class="ml-2">Close Menu</span></div>
+		{/if}
+		<div class="bg-white shadow rounded flex flex-col">
+			<GroupSidebarButton action={() => (selectedPage = 'flow')} text="Flow" />
+			<GroupSidebarButton
+				action={() => (selectedPage = 'delegation')}
+				text="Delegation"
+				icon={faPeopleArrows}
+			/>
+			<GroupSidebarButton
+				action={() => (selectedPage = 'documents')}
+				text="Documents"
+				icon={faFile}
+			/>
+			<GroupSidebarButton
+				action={() => (selectedPage = 'members')}
+				text="Members"
+				icon={faUserGroup}
+			/>
+			<GroupSidebarButton
+				action={() => (selectedPage = 'statistics')}
+				text="Statistics"
+				icon={faChartColumn}
+			/>
+			<GroupSidebarButton
+				action={() => (selectedPage = 'about')}
+				text="About"
+				icon={faCircleInfo}
+			/>
+		</div>
+		<div class="bg-white shadow rounded flex flex-col mt-6">
+			<GroupSidebarButton
+				action={() => (window.location.href = `/createpoll?id=${$page.params.groupId}`)}
+				text="Create Poll"
+				icon={faPoll}
+			/>
+			<GroupSidebarButton
+				action={() => (window.location.href = `https://meet.flowback.org/${group.jitsi_room}`)}
+				text="Video Conference"
+				icon={faVideoCamera}
+			/>
+		</div>
+		<div class="bg-white shadow rounded flex flex-col mt-6">
+			<GroupSidebarButton
+				action={() => (selectedPage = 'email')}
+				text="Send Email"
+				icon={faMailReplyAll}
+			/>
+			<GroupSidebarButton action={() => (selectedPage = 'tags')} text="Edit Tags" icon={faCog} />
+			<GroupSidebarButton
+				action={() => (window.location.href = `/creategroup?group=${$page.params.groupId}`)}
+				text="Edit Group"
+				icon={faCog}
+			/>
+		</div>
+	{/if}
 </div>

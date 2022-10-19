274ab39ae1fa388d6c5514b7679b5f6748c5399f
Kattenelvis <officialkattenelvis@gmail.com>
2022-10-19 17:19:47 +0200

    packaging

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
+        // colors: {
+        //     'blue': '#990000',
+        //     'red': "#110022"
+        // },
         extend: {},
     },
     plugins: [],

522536a5e1bb569df60b98925f12854c8ef943ce
Kattenelvis <officialkattenelvis@gmail.com>
2022-10-20 23:45:02 +0200

    fixed modal again

--- src/lib/Generic/Modal.svelte
+++ src/lib/Generic/Modal.svelte
@@ -7,9 +7,9 @@
 	class:hidden={!open}
 	id="popup-modal"
 	tabindex="-1"
-	class="bg-transparent overflow-y-auto overflow-x-hidden fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-50 md:inset-0 h-modal md:h-full"
+	class="bg-transparent overflow-y-auto overflow-x-hidden fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-50 h-modal"
 >
-	<div class="shadow-xl border border-gray-300 rounded bg-white relative p-4 w-full max-w-md h-full md:h-auto">
+	<div class="shadow-xl border border-gray-300 rounded bg-white relative p-4 w-full max-w-md h-full">
 		<div class="text-xl border-b-2 border-gray-300 border-solid">
 			<slot name="header" />
 			<CrossButton action={() => (open = false)} />

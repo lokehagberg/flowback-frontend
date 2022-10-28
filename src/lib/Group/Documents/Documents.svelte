141a729b1a7326981b5fbf6e0e1825c04a3f7e6d
Kattenelvis <officialkattenelvis@gmail.com>
2022-10-29 01:20:09 +0200

    Drive embedd

--- src/lib/Group/Documents/Documents.svelte
+++ src/lib/Group/Documents/Documents.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Folder from './Folder.svelte';
+	import ButtonPrimary from '$lib/Generic/ButtonPrimary.svelte';
+import Folder from './Folder.svelte';
 
 	let root = [
 		{
@@ -26,14 +27,20 @@
 	];
 </script>
 
-<div class="w-full bg-white flex items-center flex-col shadow rounded">
+<div class="w-full">
+	<ButtonPrimary action={() => open("https://drive.google.com/drive/u/0/folders/1tNZW9-xdClz4KHLU7zWqHB7Rfi8YyUz-")}>Gå till huvudmappen</ButtonPrimary>
+	<iframe title="Documents" class="bg-white shadow rounded p-4 mt-6 w-full h-[500px]" src="https://drive.google.com/embeddedfolderview?id=1tNZW9-xdClz4KHLU7zWqHB7Rfi8YyUz-#list"></iframe>
+</div>
+
+
+<!-- <div class="w-full bg-white flex items-center flex-col shadow rounded">
 	<div class="rounded-full border border-gray-500 p-2 w-1/2 text-center">+ Add Document</div>
 	<ul>
 		<li>
 			<Folder name="Home" files={root} expanded />
 		</li>
 	</ul>
-</div>
+</div> 
 
 <style>
 	ul {
@@ -46,4 +53,4 @@
 	li {
 		padding: 0.2em 0;
 	}
-</style>
+</style> -->

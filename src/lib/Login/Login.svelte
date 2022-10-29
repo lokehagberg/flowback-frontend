ed08008f4f3cf731c3808754653b370abb6a00cd
Kattenelvis <officialkattenelvis@gmail.com>
2022-10-29 12:02:15 +0200

    loader at login

--- src/lib/Login/Login.svelte
+++ src/lib/Login/Login.svelte
@@ -4,18 +4,21 @@
 	import type { StatusMessageInfo } from '$lib/Generic/GenericFunctions';
 	import { _ } from 'svelte-i18n';
 	import StatusMessage from '$lib/Generic/StatusMessage.svelte';
+	import Loader from '$lib/Generic/Loader.svelte';
 	// import { userInfo } from '$lib/Generic/GenericFunctions';
 
-	let username: string;
-	let password: string;
-	let status: StatusMessageInfo;
+	let username: string,
+		password: string,
+		status: StatusMessageInfo,
+		loading = false;
 
 	export let selectedPage: string;
 
 	const logIn = async () => {
+		loading = true;
 		const { json, res } = await fetchRequest('POST', 'login', { username, password }, false);
+		loading = false;
 
-		console.log(json)
 		if (!res.ok) {
 			status =
 				res.status === 400
@@ -40,22 +43,23 @@
 	};
 </script>
 
-<form class="p-6 gap-6 flex flex-col items-center" on:submit|preventDefault={logIn}>
-	
-	<TextInput label={'Email'} bind:value={username} required />
-	<TextInput label={'Password'} bind:value={password} type={'password'} required />
-	
-	<input
-	type="submit"
-	value={$_("Login")}
-	class="inline bg-blue-600 text-white pl-6 pr-6 pt-2 pb-2 mt-5 mb-5 rounded cursor-pointer"
-	>
-	
-	<StatusMessage bind:status />
-	<div
-		class="mb-4 cursor-pointer hover:underline"
-		on:click={() => (selectedPage = 'ForgotPassword')}
-	>
-		{$_('Forgot password?')}
-	</div>
-</form>
+<Loader bind:loading>
+	<form class="p-6 gap-6 flex flex-col items-center" on:submit|preventDefault={logIn}>
+		<TextInput label={'Email'} bind:value={username} required />
+		<TextInput label={'Password'} bind:value={password} type={'password'} required />
+
+		<input
+			type="submit"
+			value={$_('Login')}
+			class="inline bg-blue-600 text-white pl-6 pr-6 pt-2 pb-2 mt-5 mb-5 rounded cursor-pointer"
+		/>
+
+		<StatusMessage bind:status />
+		<div
+			class="mb-4 cursor-pointer hover:underline"
+			on:click={() => (selectedPage = 'ForgotPassword')}
+		>
+			{$_('Forgot password?')}
+		</div>
+	</form>
+</Loader>

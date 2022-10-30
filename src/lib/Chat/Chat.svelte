b8abb3b424dd866de45334e3bde12a8c6aa1eb74
Kattenelvis <officialkattenelvis@gmail.com>
2022-10-30 14:13:21 +0100

    chat

--- src/lib/Chat/Chat.svelte
+++ src/lib/Chat/Chat.svelte
@@ -5,52 +5,91 @@
 	import { faX } from '@fortawesome/free-solid-svg-icons/faX';
 	import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
 	import ButtonPrimary from '$lib/Generic/ButtonPrimary.svelte';
+	import { fetchRequest } from '$lib/FetchRequest';
+	import type { Group } from '$lib/Group/interface';
 
 	let messages: Message[] = [];
-	let chatOpen = false;
+	let chatOpen = true;
 	let message: string;
+	let groups: Group[] = [];
+	let socket: WebSocket;
 	let sendMessageToSocket: (message: string) => void;
 
 	onMount(async () => {
-		//Must be imported here to avoid "document not found" error
-		const { subscribe, sendMessage } = (await import('./Socket')).default;
-		sendMessageToSocket = sendMessage(1);
-		subscribe((e: any) => {
-			const { message, user } = JSON.parse(e);
-			messages = [...messages, { message, user }];
-		});
+		groups = await getGroups();
+		// setUpMessageSending();
 	});
+
+	const setUpMessageSending = async (selectedGroup: number) => {
+		//Must be imported here to avoid "document not found" error
+		const { createSocket, subscribe, sendMessage } = (await import('./Socket')).default;
+
+		if (socket) socket.close();
+		socket = createSocket(selectedGroup);
+		try {
+			sendMessageToSocket = sendMessage(selectedGroup, socket);
+			subscribe((e: any) => {
+				const { message, user } = JSON.parse(e);
+				messages = [...messages, { message, user }];
+			});
+		} catch (e) {}
+	};
 
 	const HandleMessageSending = async () => {
 		await sendMessageToSocket(message);
 		message = '';
 	};
+
+	const getGroups = async () => {
+		const { res, json } = await fetchRequest('GET', `group/list?joined=true&limit=100`);
+		return json.results;
+	};
 </script>
 
-<div class="fixed z-10">
-	{#if chatOpen}
-	<div class="w-full bg-white shadow rounded p-8">
-		<div on:click={() => (chatOpen = false)} class="cursor-pointer p-2">
+{#if chatOpen}
+	<div class="bg-white fixed z-10 w-full">
+		<div
+			on:click={() => (chatOpen = false)}
+			class="flex justify-between bg-white border border-gray-300 hover:border-gray-600 cursor-pointer p-2 "
+		>
+			<div class="">Chat</div>
 			<Fa icon={faX} />
 		</div>
-		{#each messages as message}
-			<div>{message.message}</div>
-		{/each}
-		<form on:submit|preventDefault={HandleMessageSending}>
-			<textarea
-				on:keypress={(e) => {
-					if (e.key === 'Enter' && !e.shiftKey) HandleMessageSending();
-				}}
-				label="write a message"
-				required
-				bind:value={message}
-			/>
-			<ButtonPrimary type="submit" />
-		</form>
-	</div>
-	{:else}
-		<div on:click={() => (chatOpen = true)} class="bg-white shadow rounded p-10 cursor-pointer">
-			<Fa icon={faPlus} />
+		<ul class="bg-white">
+			{#each messages as message}
+				<li>{message.message}</li>
+			{/each}
+		</ul>
+		<ul class="bg-white">
+			{#each groups as group}
+				<li on:click={() => setUpMessageSending(group.id)}>
+					<img
+						class="w-10 h-10"
+						src={`${import.meta.env.VITE_API}${group.image}`}
+						alt="group profile"
+					/>
+				</li>
+			{/each}
+		</ul>
+		<div class="w-full bg-white shadow rounded p-8 w-full">
+			<form on:submit|preventDefault={HandleMessageSending}>
+				<textarea
+					on:keypress={(e) => {
+						if (e.key === 'Enter' && !e.shiftKey) HandleMessageSending();
+					}}
+					label="write a message"
+					required
+					bind:value={message}
+				/>
+				<ButtonPrimary type="submit" />
+			</form>
 		</div>
-	{/if}
-</div>
+	</div>
+{:else}
+	<div
+		on:click={() => (chatOpen = true)}
+		class="fixed z-30 bg-white shadow rounded p-6 top-1/4 cursor-pointer"
+	>
+		<Fa icon={faPlus} />
+	</div>
+{/if}
--- src/lib/Chat/Socket.ts
+++ src/lib/Chat/Socket.ts
@@ -2,33 +2,38 @@ import { writable } from 'svelte/store';
 
 const messageStore = writable('');
 
-let socket: WebSocket;
+//Maybe TODO: Make this class based, maybe a "SocketManager" class.
+const createSocket = (id:number) => {
+	let socket: WebSocket;
+	
+	const token = localStorage.getItem('token') || '';
+	socket = new WebSocket(`${import.meta.env.VITE_WEBSOCKET_API}/chat/ws/group/${id}?token=${token}`);
+	
+	socket.onopen = (event) => {
+		console.log('[open] Connection established');
+	};
+	
+	socket.onmessage = (event) => {
+		messageStore.set(event.data);
+		console.log(`[message] Data received from server: ${event.data}`);
+	};
+	
+	socket.onclose = (event) => {
+		if (event.wasClean) {
+			console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+		} else {
+			console.warn('[close] Connection died');
+		}
+	};
+	
+	socket.onerror = (error) => {
+		console.error(`[error] ${error}`);
+	};
 
-const token = localStorage.getItem('token') || '';
-socket = new WebSocket(`${import.meta.env.VITE_WEBSOCKET_API}/chat/ws/group/14?token=${token}`);
+	return socket
+}
 
-socket.onopen = (event) => {
-	console.log('[open] Connection established');
-};
-
-socket.onmessage = (event) => {
-	messageStore.set(event.data);
-	console.log(`[message] Data received from server: ${event.data}`);
-};
-
-socket.onclose = (event) => {
-	if (event.wasClean) {
-		console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-	} else {
-		console.warn('[close] Connection died');
-	}
-};
-
-socket.onerror = (error) => {
-	console.error(`[error] ${error}`);
-};
-
-const sendMessage = (target: number) => {
+const sendMessage = (target: number, socket:WebSocket) => {
 	return (message: string) => {
 		
 		if (socket.readyState <= 1) {
@@ -37,4 +42,4 @@ const sendMessage = (target: number) => {
 	};
 };
 
-export default { subscribe: messageStore.subscribe, sendMessage };
+export default { createSocket, subscribe: messageStore.subscribe, sendMessage };

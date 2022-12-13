171831c0a589793f8d3e78134a2acb6294b7d81f
Kattenelvis <officialkattenelvis@gmail.com>
2022-12-13 16:23:26 +0100

    no progress on pie chart

--- src/lib/Poll/Statistics.svelte
+++ src/lib/Poll/Statistics.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { Pie } from 'svelte-chartjs';
 	import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from 'chart.js';
 	import { onMount } from 'svelte';
@@ -8,7 +8,7 @@
 	export let votes: number[] = [1],
 		labels: string[] = [''];
 
-	let data = {
+	const data = {
 		labels,
 		datasets: [
 			{
@@ -22,23 +22,73 @@
 		]
 	};
 
-	console.log(data, 'datadfsasfzfdsdasfstadatfsdgadtadata');
+	console.log(data, 'datadfsafsfsfsassfzfdsdasfddatfsdgadtadata');
 
-	onMount(() => {
-		data = {
-			labels,
-			datasets: [
-				{
-					label: 'Results',
-					fill: true,
-					//TODO: Make sure to make these colors editable when we're adding stylesheets
-					backgroundColor: ['#0157BE', '#9333EA', '#93C5FD', '#D8B4FE'],
-					borderColor: ['#FFFFFF'],
-					data: votes
-				}
-			]
-		};
-	});
+	// onMount(() => {
+	// 	data = {
+	// 		labels,
+	// 		datasets: [
+	// 			{
+	// 				label: 'Results',
+	// 				fill: true,
+	// 				//TODO: Make sure to make these colors editable when we're adding stylesheets
+	// 				backgroundColor: ['#0157BE', '#9333EA', '#93C5FD', '#D8B4FE'],
+	// 				borderColor: ['#FFFFFF'],
+	// 				data: votes
+	// 			}
+	// 		]
+	// 	};
+	// });
 </script>
 
-<Pie {data} options={{ responsive: true }} />
+<Pie {data} options={{ responsive: true }} /> -->
+
+
+<!-- <script>
+	import { Line } from 'svelte-chartjs'
+	import { Chart as ChartJS } from 'chart.js';
+	ChartJS.register(Line)
+  
+  </script>
+  
+  
+  <Line data={{labels:['hi', '2', '4'], datasets: [
+	{
+		label:"haw",
+		backgroundColor: "#444444",
+		fill:-1,
+		data:[1,2,3]
+	}
+  ]}} /> -->
+
+
+<!-- <script>
+	import { Line } from 'svelte-chartjs'
+  </script>
+  
+  <Line data={{labels:['hi'], datasets: [
+	{
+		data:[1,2,3]
+	}
+  ]}} /> -->
+
+
+<!-- 
+  <script>
+	import { Pie } from 'svelte-chartjs';
+	import { data } from './data.js';
+  
+	import {
+	  Chart as ChartJS,
+	  Title,
+	  Tooltip,
+	  Legend,
+	  ArcElement,
+	  CategoryScale,
+	} from 'chart.js';
+  
+	ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+  </script>
+  
+  <Pie {data} options={{ responsive: true }} />
+   -->

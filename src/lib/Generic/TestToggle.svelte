3246fdb3d547721c2063be2798fe27485a85fca9
Kattenelvis <officialkattenelvis@gmail.com>
2023-01-03 22:43:36 +0100

    test button

---
+++ src/lib/Generic/TestToggle.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import ButtonPrimary from './ButtonPrimary.svelte';
+
+	export let action = () => {},
+		loopTime = 1000;
+	let interval: any,
+		toggled = false;
+
+	const repeatAction = () => {
+		interval = setInterval(() => {
+			action();
+		}, loopTime);
+	};
+</script>
+
+<ButtonPrimary
+	action={toggled
+		? () => {
+				toggled = false;
+				clearInterval(interval);
+		  }
+		: () => {
+				toggled = true;
+				repeatAction();
+		  }}
+>
+	Toggle
+</ButtonPrimary>

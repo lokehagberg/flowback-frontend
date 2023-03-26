59d019178bca3a7530bcf23592bd2d37f0b3f490
Kattenelvis <officialkattenelvis@gmail.com>
2023-03-26 13:28:00 +0200

    reading notifications

--- src/lib/Header/Notifications.svelte
+++ src/lib/Header/Notifications.svelte
@@ -4,14 +4,13 @@
 	import { _ } from 'svelte-i18n';
 	import { fetchRequest } from '$lib/FetchRequest';
 	import { onMount } from 'svelte';
-	import Button from '$lib/Generic/Button.svelte';
 	import type { notification } from './Notification';
 
-	let notifications:notification[];
+	let notifications: notification[], hovered:number[] = [];
 
 	const getNotifications = async () => {
 		const { json, res } = await fetchRequest('GET', 'notification/list');
-		notifications = json.results
+		notifications = json.results;
 	};
 
 	const closeWindowWhenClickingOutside = () => {
@@ -24,7 +23,11 @@
 				notificationsOpen = false;
 			}
 		});
-	}
+	};
+
+	const readNotification = async (id: number) => {
+		await fetchRequest('POST', 'notification/read', { notification_ids: [id], read: true });
+	};
 
 	onMount(() => {
 		getNotifications();
@@ -34,20 +37,34 @@
 	let notificationsOpen = false;
 </script>
 
-<div id ="notifications-list" on:click={() => (notificationsOpen = !notificationsOpen)}>
+<div id="notifications-list" on:click={() => (notificationsOpen = !notificationsOpen)}>
 	<Fa icon={faBell} size={'1.4x'} />
 </div>
 
 {#if notificationsOpen}
-	<div class="absolute right-0 top-full bg-white select-none shadow slide-animation" id ="notifications-list">
+	<div
+		class="absolute right-0 top-full bg-white select-none shadow slide-animation"
+		id="notifications-list"
+	>
+	{#if notifications.length > 0}
 		{#each notifications as notification}
-		<!-- on:click={notification.action} -->
+			<!-- on:click={notification.action} -->
 			<div
-				class="cursor-pointer pt-3 pb-3 pr-10 pl-6 border-b border-gray-200 border hover:shadow hover:bg-blue-300 transition-shadow transition-colors"
+				class="cursor-pointer pt-3 pb-3 pr-10 pl-6 border-b border-gray-200 border hover:shadow hover:bg-blue-300 transition-all"
+				class:bg-gray-200={hovered.find(hover => hover === notification.id)}
+				on:focus={() => {}}
+				on:mouseover={() => {
+					hovered.push(notification.id)
+					hovered = hovered
+					readNotification(notification.id)}
+					}
 			>
 				{$_(notification.message)}
 			</div>
 		{/each}
+		{:else}
+		<div class="pt-3 pb-3 pr-10 pl-6 border-b border-gray-200 border cursor-default">{$_("No notifications")}</div>
+		{/if}
 	</div>
 {/if}
 

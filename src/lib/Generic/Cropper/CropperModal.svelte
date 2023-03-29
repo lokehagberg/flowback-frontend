97a2bf250e603e744e5992ad5f23fa79c8fcdf8e
Kattenelvis <officialkattenelvis@gmail.com>
2023-03-29 18:06:59 +0200

    Nicer looking cropper

--- src/lib/Generic/Cropper/CropperModal.svelte
+++ src/lib/Generic/Cropper/CropperModal.svelte
@@ -2,6 +2,7 @@
 	import Cropper from 'svelte-easy-crop';
 	import Button from '../Button.svelte';
 	import getCroppedImg from './canvasUtils';
+	import { _ } from 'svelte-i18n';
 
 	let crop = { x: 0, y: 0 };
 	let zoom = 1;
@@ -43,8 +44,8 @@
 	}
 </script>
 
-<div class="z-50 fixed p-4 bg-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-	<h1>Cropping</h1>
+<div class="z-50 w-[80%] md:w-1/2 max-w-2xl fixed p-4 bg-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 shadow rounded-md">
+	<h1 class="text-2xl mb-4">{$_("Cropping")}</h1>
 	<div style="position: relative; width: 100%; height: 300px;">
 		<Cropper
 			showGrid={false}
@@ -56,13 +57,13 @@
 			aspect={1}
 		/>
 	</div>
-	<h2>Preview</h2>
+	<!-- <h2>Preview</h2> -->
 	<div class="prof-pic-wrapper hidden">
 		<img bind:this={profilePicture} class="prof-pic" src={image} alt="Profile example" {style} />
 	</div>
 	{#if croppedImage}
-		<h2>Cropped Output</h2>
-		<img src={croppedImage} alt="Cropped profile" /><br />
+		<h2 class="hidden">{$_("Cropped Output")}</h2>
+		<img src={croppedImage} class="hidden" alt="Cropped profile" /><br />
 	{/if}
 	<!-- <h2>Preview</h2> -->
 	<!-- <div class="prof-pic-wrapper hidden">
@@ -73,6 +74,7 @@
 		<!-- <h2>Cropped Output</h2>
 		<img src={croppedImage} alt="Cropped profile" /><br /> -->
 	{:else}
+	<div class="mt-6 mb-2">
 		<Button
 			buttonStyle="primary"
 			action={async () => {
@@ -88,9 +90,10 @@
 				console.log('HIIIII', userEdit.profile_image_file);
 				console.log('HIIIII', userEdit.banner_image_file);
 				confirmAction();
-			}}>Confirm</Button
+			}}>{$_("Confirm")}</Button
 		>
-		<Button buttonStyle="secondary" action={cancelAction}>Cancel</Button>
+		<Button buttonStyle="secondary" action={cancelAction} Class="bg-gray-500">{$_("Cancel")}</Button>
+	</div>
 	{/if}
 </div>
 

d88e059b91e0e19764a3e4331fdd99ca2dd761d3
Kattenelvis <officialkattenelvis@gmail.com>
2023-04-27 19:52:48 +0200

    a

--- src/lib/Login/Login.svelte
+++ src/lib/Login/Login.svelte
@@ -46,7 +46,6 @@
 			type="submit"
 			value={$_('Login')}
 			class="inline bg-blue-600 text-white pl-6 pr-6 pt-2 pb-2 mt-5 mb-5 rounded cursor-pointer"
-			label="Login"
 		/>
 
 		<StatusMessage bind:status />

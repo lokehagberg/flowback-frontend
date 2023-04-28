c773dbebfcd9244cb6ee1c79fc3189419d27c35e
Kattenelvis <officialkattenelvis@gmail.com>
2023-04-28 13:59:32 +0200

    Cookies

--- src/lib/Generic/Cookies.svelte
+++ src/lib/Generic/Cookies.svelte
@@ -14,16 +14,16 @@
 		localStorage.setItem('cookies-consent', 'true');
 	};
 
+    //TODO: Maybe implement this in the future (using google analytics)
 	const denyCookies = () => {
 		hasClicked = true;
 		localStorage.setItem('cookies-consent', 'false');
 	};
 </script>
 
-<div class="bg-white p-6 fixed bottom-3 left-1/2 -translate-x-1/2 w-3/4" class:hidden={hasClicked}>
+<div class="bg-white p-6 fixed bottom-3 left-1/2 -translate-x-1/2 border-gray-200 shadow z-50" class:hidden={hasClicked}>
 	<span
-		>{$_('We use cookies to improve the experience of Flowback. Do you consent to cookies?')}
+		>{$_('We use cookies to improve the experience of Flowback')}
 	</span>
-	<Button action={acceptCookies}>{$_('Yes')}</Button>
-	<Button action={denyCookies}>{$_('No')}</Button>
+	<Button action={acceptCookies} Class="ml-2">{$_('Ok')}</Button>
 </div>

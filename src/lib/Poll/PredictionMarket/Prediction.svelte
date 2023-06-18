ed515fdd0d61344cc888c293e8344bf47f7ff13e
Kattenelvis <officialkattenelvis@gmail.com>
2023-06-19 00:20:18 +0200

    Prediction markets

---
+++ src/lib/Poll/PredictionMarket/Prediction.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let prediction:any;
+</script>
+
+{prediction.title}
--- src/lib/Poll/PredictionMarket/Predictions.svelte
+++ src/lib/Poll/PredictionMarket/Predictions.svelte
@@ -1,6 +1,35 @@
 <script lang="ts">
+	import { fetchRequest } from '$lib/FetchRequest';
+	import { onMount } from 'svelte';
+	import json from './PredictionTest.json';
+	import { page } from '$app/stores';
+	import Loader from '$lib/Generic/Loader.svelte';
+	import Prediction from './Prediction.svelte';
 
+	let loading = false,
+		predictions: any[] = [];
+
+	const getDelegateHistory = async () => {
+		loading = true;
+		// const { res, json } = await fetchRequest('GET', `group/poll/pool/${history}/poll/votes`);
+		loading = false;
+		// const { res, json } = await fetchRequest(
+		// 	'GET',
+		// 	`group/${$page.params.groupId}/delegate/pools?id=${history}`
+		// );
+		// predictions = json.results[0];
+        predictions = json
+	};
+
+	onMount(() => {
+		getDelegateHistory();
+	});
 </script>
 
-<div>
-</div>
+<Loader bind:loading>
+	<ul>
+		{#each predictions as prediction}
+			<li><Prediction {prediction}/></li>
+		{/each}
+	</ul>
+</Loader>

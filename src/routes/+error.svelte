ac8deb11ea054065bdebae918c8363ba389104b1
Kattenelvis <officialkattenelvis@gmail.com>
2023-09-19 19:32:32 +0200

    added 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Layout from "$lib/Generic/Layout.svelte";
+
+</script>
+<Layout >
+
+    <h1>404 not found</h1>
+<div class="text-center">No democracy found here</div>
+
+</Layout>

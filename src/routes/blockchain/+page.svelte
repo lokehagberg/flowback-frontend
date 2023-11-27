c74a2fcdbcc64146e2089b30ade0c1398bd6384c
Kattenelvis <officialkattenelvis@gmail.com>
2023-11-27 14:53:22 +0100

    created blockchain page

---
+++ src/routes/blockchain/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Layout from '$lib/Generic/Layout.svelte';
+	import { test } from '$lib/Blockchain/javascript/test.js';
+	import Button from '$lib/Generic/Button.svelte';
+</script>
+
+<Layout>
+	<div class="p-6">
+		<Button action={test}>Make Wallet</Button>
+	</div>
+</Layout>
--- src/routes/kanban/+page.svelte
+++ src/routes/kanban/+page.svelte
@@ -1,6 +1,5 @@
 <script>
-	import {test} from '$lib/Blockchain/javascript/test.js';
-	import Button from '$lib/Generic/Button.svelte';
+
 	import Layout from '$lib/Generic/Layout.svelte';
 	import Kanban from '$lib/Group/Kanban/Kanban.svelte';
 	import { _ } from 'svelte-i18n';
@@ -15,4 +14,3 @@
 	<Kanban type="home" Class="w-[1000px]" />
 </Layout>
 
-<Button action={test}>Make Wallet</Button>

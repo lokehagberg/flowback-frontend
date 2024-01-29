ae63b2044b81954620c0f207f51e0e05c7ddd91c
Kattenelvis <officialkattenelvis@gmail.com>
2024-01-29 17:56:17 +0100

    blockchain progress

--- src/lib/Blockchain/javascript/delegationsBlockchain.ts
+++ src/lib/Blockchain/javascript/delegationsBlockchain.ts
@@ -3,14 +3,15 @@ import contractABI from './contractDelegationsABI.json';
 
 
 const getContract = (privateKey:string) => {
-	const provider = new ethers.providers.InfuraProvider(import.meta.env.INFURA_API_KEY);
+	const provider = new ethers.providers.InfuraProvider('sepolia', import.meta.env.INFURA_API_KEY);
 
 	const wallet = new ethers.Wallet(privateKey, provider);
 
 	//GAS LIMIT ERROR
 	// const contractAddress = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85';
 
-	const contractAddress = '0x0f021dba3e86994176da8abb497e5a6380439147';
+	// const contractAddress = '0x0f021dba3e86994176da8abb497e5a6380439147';
+	const contractAddress = '0x616F8F6C731f805C4Ae7D0D315cD97877F99745a';
 
 	return new ethers.Contract(contractAddress, contractABI, wallet);
 };
@@ -20,10 +21,10 @@ export const becomeMemberOfGroup = async (groupId: number, privateKey:string) =>
 	const contract = getContract(privateKey);
 	
 	// Set gasPrice value
-	const gasPrice = ethers.utils.parseUnits('20', 'gwei');
+	const gasPrice = ethers.utils.parseUnits('0.000000001', 'gwei');
 	
 	// Update transaction options with gasPrice
-	const tx = await contract.giveRightToVote(groupId.toLocaleString(), { gasLimit: 500000, gasPrice });
+	const tx = await contract.giveRightToVote(groupId.toLocaleString(), { gasLimit: 21204, gasPrice });
 	
 	
 	// Adjust the timeout based on your requirements
--- src/lib/Blockchain/javascript/newWallet.ts
+++ src/lib/Blockchain/javascript/newWallet.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 
 export const newWallet = async () => {
-	const provider = new ethers.providers.InfuraProvider(import.meta.env.INFURA_API_KEY);
+	const provider = new ethers.providers.InfuraProvider('sepolia', import.meta.env.INFURA_API_KEY);
 
 	// const balance = await provider.getBalance("0x857bA91346bB5425A093278b2ccfFce74180d82B")
 	// console.log(ethers.utils.formatEther(balance), "BALANCE")

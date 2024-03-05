58a6e9e22008dbaf6d4325ff879cb4e2c83c1142
Kattenelvis <officialkattenelvis@gmail.com>
2024-03-05 14:17:29 +0100

    ai work

--- src/routes/ai/+page.svelte
+++ src/routes/ai/+page.svelte
@@ -1,18 +1,29 @@
-<script>
+<script lang="ts">
 	import { fetchRequest } from '$lib/FetchRequest';
+	import Button from '$lib/Generic/Button.svelte';
 	import Layout from '$lib/Generic/Layout.svelte';
+	import TextInput from '$lib/Generic/TextInput.svelte';
 	import { onMount } from 'svelte';
 
-	const getAI = async () => {
-		const { res, json } = await fetchRequest('GET', 'ai/test');
-		console.log(json);
-	};
+	let prompt = '',
+		response: string[] = [];
 
-	onMount(() => {
-		getAI();
-	});
+	const getAI = async () => {
+		const { res, json } = await fetchRequest('POST', 'ai/test', { prompt });
+		response = json;
+	};
 </script>
 
 <Layout>
 	<div>welcome to AI</div>
+
+	<TextInput label="AI prompt" bind:value={prompt} />
+	<Button action={getAI} />
+	<div>
+		{#each response as item}
+			<div class="p-4 dark:bg-darkobject dark:text-white">
+				{item}
+			</div>
+		{/each}
+	</div>
 </Layout>

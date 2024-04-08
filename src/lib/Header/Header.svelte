e65d71f97f7a820d2ff133c070fc12de8f4c3d85
Kattenelvis <officialkattenelvis@gmail.com>
2024-04-08 22:49:45 +0200

    sun icon

--- src/lib/Header/Header.svelte
+++ src/lib/Header/Header.svelte
@@ -20,7 +20,7 @@
 	import Fa from 'svelte-fa/src/fa.svelte';
 	import { faMoon } from '@fortawesome/free-solid-svg-icons/faMoon';
 	// import { accountsStore } from '$lib/Account/stores';
-	import { faBurst, faCircle, faCoins } from '@fortawesome/free-solid-svg-icons';
+	import { faCoins } from '@fortawesome/free-solid-svg-icons';
 
 	let sideHeaderOpen = false,
 		profileImage = DefaultPFP,
@@ -137,7 +137,20 @@
 					}}
 				>
 					{#if darkMode}
-						<Fa icon={faBurst} />
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="30" height="30">
+						<!-- Sun rays -->
+					   
+						<line x1="50" y1="10" x2="50" y2="90" stroke="white" stroke-width="8"/>
+						<line x1="10" y1="50" x2="90" y2="50" stroke="white" stroke-width="8"/>
+						<line x1="28" y1="28" x2="72" y2="72" stroke="white" stroke-width="8"/>
+						<line x1="72" y1="28" x2="28" y2="72" stroke="white" stroke-width="8"/>
+						<line x1="22" y1="78" x2="78" y2="22" stroke="white" stroke-width="8"/>
+						<line x1="78" y1="78" x2="22" y2="22" stroke="white" stroke-width="8"/>
+						<line x1="28" y1="72" x2="72" y2="28" stroke="white" stroke-width="8"/>
+						<line x1="72" y1="72" x2="28" y2="28" stroke="white" stroke-width="8"/>
+						<!-- Sun center -->
+						<circle cx="50" cy="50" r="25" fill="white" />
+					  </svg>
 					{:else}
 						<Fa icon={faMoon} />
 					{/if}

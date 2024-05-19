48c2d726dcb0352d57603c24ebbcf909f66ba114
EllenLng <_ellen@live.se>
2024-05-19 12:29:23 +0200

    votefunc

--- src/lib/Blockchain/javascript/pollsBlockchain.ts
+++ src/lib/Blockchain/javascript/pollsBlockchain.ts
@@ -1,204 +1,198 @@
-
 import { ethers } from 'ethers';
 import contractABI from './contractABI.json';
 
 interface Window {
-  ethereum?: import('ethers').providers.ExternalProvider;
+	ethereum?: import('ethers').providers.ExternalProvider;
 }
 
-  async function getUser() {
-    if (window.ethereum) {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      console.log('Address:', address);
-      //console.log("GetCode:", await provider.getCode("0xf43205cD2E7Ab7416D73cCcFC30cD5d980c9A31a"));
+async function getUser() {
+	if (window.ethereum) {
+		await window.ethereum.request({ method: 'eth_requestAccounts' });
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		const address = await signer.getAddress();
+		console.log('Address:', address);
 
-      return signer;
-    } else {
-      console.log('MetaMask is not available');
-      throw new Error('MetaMask is not available');
-    }
-  }
-  
-  const getContract = async () => {
-    const signer = await getUser();
-    const contractAddress = '0x0fDD2AD1aEE84C91DEb80c25993c0bEde05987A3'; //use this address
-    return new ethers.Contract(contractAddress, contractABI, signer);
-  };
-  
+		return signer;
+	} else {
+		console.log('MetaMask is not available');
+		throw new Error('MetaMask is not available');
+	}
+}
 
-
+const getContract = async () => {
+	const signer = await getUser();
+	const contractAddress = '0xDeE004347e6C7D2c7B2E2e26ef9Cdab1B1838F52'; //use this address
+	return new ethers.Contract(contractAddress, contractABI, signer);
+};
 
 //----------------------------TODO update with inputs ---------------------------------------------------
 
 export const createPoll= async () => {
 
-	const contract = await getContract();
-  const nowInSeconds = Math.floor(Date.now() / 1000);
-  const oneDayInSeconds = 24 * 60 * 60; 
-  try {
-        const tx = await contract.createPoll(
-          "title",
-          "tag",
-          1, //group
-          nowInSeconds, //pollstartdate
-          nowInSeconds + oneDayInSeconds, //proposalenddate
-          nowInSeconds + 2 * oneDayInSeconds, //votingstartdate
-          nowInSeconds + 3 * oneDayInSeconds, //delegateenddate
-          nowInSeconds + 4 * oneDayInSeconds, //enddate
-        );
-	    
-        const txReceipt = await tx.wait({ timeout: 40000 }).catch((error:any) => {
-        console.error('Error waiting for transaction:', error);
-      });
-
-      if (txReceipt && txReceipt.status === 1) {
-        console.log('Transaction successful');
-        const logs = txReceipt.logs;
-        const parsedLogs = logs.map((log: any) => contract.interface.parseLog(log));
-        const pollCreatedEvents = parsedLogs.filter(log => log.name === 'PollCreated');
-        const PollCreatedEvent = pollCreatedEvents.length > 0 ? pollCreatedEvents[0] : undefined;
-
-        console.log(PollCreatedEvent)
-            if (PollCreatedEvent) {
-                const pollId = parseInt(PollCreatedEvent.args.pollId);
-                const title = PollCreatedEvent.args.title;
-                console.log(`Poll created with title ${title} and id ${pollId}`);
-            }
-        }
-  } catch (error) {
-
-    if (error instanceof Error) {
-      console.error('Error waiting for transaction:', error.message);
-    } else {
-      console.error('An unexpected error occurred:', error);
-    }
-  }
-};
-
-export const getPoll= async () => {
-
-	const contract = await getContract();
-  try {
-    const tx = await contract.getPoll(1); //id 1
-    console.log(`Poll with id ${tx.pollId}:`);
-    console.log(`Title: ${tx.title}`);
-    console.log(`Tag: ${tx.tag}`);
-    console.log(`Startdate: ${tx.pollStartDate}`);
-    console.log(`Enddate: ${tx.endDate}`);
-    console.log(`Proposalcount: ${tx.proposalCount}`);
-
-    //delegateEndDate, group, proposalEndDate
-    
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error('Error waiting for transaction:', error.message);
-    } else {
-      console.error('An unexpected error occurred:', error);
-    }
-  }
-};
-export const createProposal= async () => {
-
-	const contract = await getContract();
-  try {
-        const tx = await contract.addProposal(
-          1, //pollid
-          "description", //description
-        );
-	    
-        const txReceipt = await tx.wait({ timeout: 40000 }).catch((error:any) => {
-        console.error('Error waiting for transaction:', error);
-      });
-
-      if (txReceipt && txReceipt.status === 1) {
-        console.log('Transaction successful');
-        const logs = txReceipt.logs;
-        const parsedLogs = logs.map((log: any) => contract.interface.parseLog(log));
-        const ProposalCreatedEvent = parsedLogs.find(log => log.name === 'ProposalAdded');
-            if (ProposalCreatedEvent) {
-                const pollId = parseInt(ProposalCreatedEvent.args.pollId);
-                const proposalId = ProposalCreatedEvent.args.proposalId;
-                const description = ProposalCreatedEvent.args.description;
-                console.log(`Proposal with id ${proposalId} created on poll with id ${pollId}: PROPOSAL: ${description}`);
-            }
-        }
-  } catch (error) {
-
-    if (error instanceof Error) {
-      console.error('Error waiting for transaction:', error.message);
-    } else {
-      console.error('An unexpected error occurred:', error);
-    }
-  }
-};
-export const getProposalsOnPoll= async () => {
   const contract = await getContract();
-  try {
-    const tx = await contract.getProposals(1); //poll id 1
-    console.log(tx);
-    console.log("PROPOSALS ON POLLID ", 1); //change when inputs is done
-    tx.forEach(element => {
-      console.log(`Proposal id ${element.proposalId}: "${element.description}" votes: ${element.voteCount}`)
-    });
-    
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error('Error waiting for transaction:', error.message);
-    } else {
-      console.error('An unexpected error occurred:', error);
+    const nowInSeconds = Math.floor(Date.now() / 1000);
+    const oneDayInSeconds = 24 * 60 * 60; 
+    try {
+          const tx = await contract.createPoll(
+            "title",
+            "tag",
+            1, //group
+            nowInSeconds, //pollstartdate
+            nowInSeconds + oneDayInSeconds, //proposalenddate
+            nowInSeconds, //votingstartdate
+            nowInSeconds + 3 * oneDayInSeconds, //delegateenddate
+            nowInSeconds + 4 * oneDayInSeconds, //enddate
+          );
+  
+          const txReceipt = await tx.wait({ timeout: 40000 }).catch((error:any) => {
+          console.error('Error waiting for transaction:', error);
+        });
+  
+        if (txReceipt && txReceipt.status === 1) {
+          console.log('Transaction successful');
+          const logs = txReceipt.logs;
+          const parsedLogs = logs.map((log: any) => contract.interface.parseLog(log));
+			const pollCreatedEvents = parsedLogs.filter((log:any) => log.name === 'PollCreated');
+          const PollCreatedEvent = pollCreatedEvents.length > 0 ? pollCreatedEvents[0] : undefined;
+  
+          console.log(PollCreatedEvent)
+              if (PollCreatedEvent) {
+                  const pollId = parseInt(PollCreatedEvent.args.pollId);
+                  const title = PollCreatedEvent.args.title;
+				console.log(`Poll created with title ${title} and id ${pollId}`);
+              }
+          }
+    } catch (error) {
+  
+      if (error instanceof Error) {
+        console.error('Error waiting for transaction:', error.message);
+      } else {
+        console.error('An unexpected error occurred:', error);
+      }
     }
-  }
-}
-export const getPollResults= async () => {
-  const contract = await getContract();
-  try {
-    const tx = await contract.getPollResults(1); //poll id 1
-    console.log(tx)
-    
-    
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error('Error waiting for transaction:', error.message);
-    } else {
-      console.error('An unexpected error occurred:', error);
-    }
-  }
+  };
 
-}
-export const vote= async () => {
-
+export const getPoll = async (id: number) => {
 	const contract = await getContract();
-  try {
-        const tx = await contract.vote(
-          1, //pollid
-          1, //proposalid
-        );
-	    
-        const txReceipt = await tx.wait({ timeout: 40000 }).catch((error:any) => {
-        console.error('Error waiting for transaction:', error);
-      });
+	try {
+		const tx = await contract.getPoll(id);
+		console.log(`Poll with id ${tx.pollId}:`);
+		console.log(`Title: ${tx.title}`);
+		console.log(`Tag: ${tx.tag}`);
+		console.log(`Startdate: ${tx.pollStartDate}`);
+		console.log(`Voting date: ${tx.votingStartDate}`);
+		console.log(`Enddate: ${tx.endDate}`);
+		console.log(`Proposalcount: ${tx.proposalCount}`);
 
-      if (txReceipt && txReceipt.status === 1) {
-        console.log('Transaction successful');
-        const logs = txReceipt.logs;
-        const parsedLogs = logs.map((log: any) => contract.interface.parseLog(log));
-        const VoteSubmittedCreatedEvent = parsedLogs.find(log => log.name === 'VoteSubmitted');
-            if (VoteSubmittedCreatedEvent) {
-                const pollId = parseInt(VoteSubmittedCreatedEvent.args.pollId);
-                const voter= VoteSubmittedCreatedEvent.args.voter;
-                const votesForProposal = VoteSubmittedCreatedEvent.args.votesForProposal;
-                console.log(`Vote has been cast on poll with id ${pollId} by voter: ${voter}, votes for proposal is now ${votesForProposal}`);
-            }
-        }
-  } catch (error) {
+		//delegateEndDate, group, proposalEndDate
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error('Error waiting for transaction:', error.message);
+		} else {
+			console.error('An unexpected error occurred:', error);
+		}
+	}
+};
+export const createProposal = async (_pollId: number) => {
+	const contract = await getContract();
+	try {
+		const tx = await contract.addProposal(
+			_pollId, //pollid
+			'description' //description
+		);
 
-    if (error instanceof Error) {
-      console.error('Error waiting for transaction:', error.message);
-    } else {
-      console.error('An unexpected error occurred:', error);
-    }
-  }
+		const txReceipt = await tx.wait({ timeout: 40000 }).catch((error: any) => {
+			console.error('Error waiting for transaction:', error);
+		});
+
+		if (txReceipt && txReceipt.status === 1) {
+			console.log('Transaction successful');
+			const logs = txReceipt.logs;
+			const parsedLogs = logs.map((log: any) => contract.interface.parseLog(log));
+			const ProposalCreatedEvent = parsedLogs.find((log:any) => log.name === 'ProposalAdded');
+			if (ProposalCreatedEvent) {
+				const pollId = parseInt(ProposalCreatedEvent.args.pollId);
+				const proposalId = ProposalCreatedEvent.args.proposalId;
+				const description = ProposalCreatedEvent.args.description;
+				console.log(
+					`Proposal with id ${proposalId} created on poll with id ${pollId}: PROPOSAL: ${description}`
+				);
+			}
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error('Error waiting for transaction:', error.message);
+		} else {
+			console.error('An unexpected error occurred:', error);
+		}
+	}
+};
+export const getProposalsOnPoll = async (id:number) => {
+	const contract = await getContract();
+	try {
+		const tx = await contract.getProposals(id);
+		console.log(tx);
+		console.log('PROPOSALS ON POLLID ', id); //change when inputs is done
+		tx.forEach((element:any) => {
+			console.log(
+				`Proposal id ${element.proposalId}: "${element.description}" votes: ${element.voteCount}`
+			);
+		});
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error('Error waiting for transaction:', error.message);
+		} else {
+			console.error('An unexpected error occurred:', error);
+		}
+	}
+};
+export const getPollResults = async (id:number) => {
+	const contract = await getContract();
+	try {
+		const tx = await contract.getPollResults(id);
+		console.log(tx);
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error('Error waiting for transaction:', error.message);
+		} else {
+			console.error('An unexpected error occurred:', error);
+		}
+	}
+};
+
+export const vote = async (_pollId: number, _proposalId: number) => {
+	const contract = await getContract();
+	try {
+		const tx = await contract.vote(
+			_pollId, //pollid
+			_proposalId //proposalid
+		);
+
+		const txReceipt = await tx.wait({ timeout: 40000 }).catch((error: any) => {
+			console.error('Error waiting for transaction:', error);
+		});
+
+		if (txReceipt && txReceipt.status === 1) {
+			console.log('Transaction successful');
+			const logs = txReceipt.logs;
+			const parsedLogs = logs.map((log: any) => contract.interface.parseLog(log));
+			const VoteSubmittedEvent = parsedLogs.find((log:any) => log.name === 'VoteSubmitted');
+			if (VoteSubmittedEvent) {
+        console.log(VoteSubmittedEvent.args)
+				const pollId = parseInt(VoteSubmittedEvent.args.pollId);
+				const voter = VoteSubmittedEvent.args.voter;
+				const votesForProposal = VoteSubmittedEvent.args.votesForProposal;
+				console.log(
+					`Vote has been cast on a proposal on poll with id ${pollId} by voter: ${voter}, votes for that proposal is now ${votesForProposal}`
+				);
+			}
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error('Error waiting for transaction:', error.message);
+		} else {
+			console.error('An unexpected error occurred:', error);
+		}
+	}
 };
--- src/routes/blockchain/+page.svelte
+++ src/routes/blockchain/+page.svelte
@@ -1,139 +1,243 @@
 <script>
-    import Layout from '$lib/Generic/Layout.svelte';
-    import { test } from '$lib/Blockchain/javascript/test';
-    import Button from '$lib/Generic/Button.svelte';
-    import { onMount } from 'svelte';
-    import { 
-        createPoll, 
-        getPoll, 
-        createProposal, 
-        getProposalsOnPoll, 
-        vote,
-		getPollResults,
-     } from '$lib/Blockchain/javascript/pollsBlockchain';
-    import {
-        createPrediction,
-        getPredictionsOnPoll,
-        createPredictionBet,
-        getPredictionBets
-    } from '$lib/Blockchain/javascript/predictionsBlockchain';
-    import { 
-        becomeMemberOfGroup, 
-        removeGroupMembership,
-        isUserMemberInGroup,
-        getGroupsUserIsMemberIn 
-    } from '$lib/Blockchain/javascript/rightToVote';
+	import Layout from '$lib/Generic/Layout.svelte';
+	import { test } from '$lib/Blockchain/javascript/test';
+	import Button from '$lib/Generic/Button.svelte';
+	import { onMount } from 'svelte';
+	import {
+		createPoll,
+		getPoll,
+		createProposal,
+		getProposalsOnPoll,
+		vote,
+		getPollResults
+	} from '$lib/Blockchain/javascript/pollsBlockchain';
+	import {
+		createPrediction,
+		getPredictionsOnPoll,
+		createPredictionBet,
+		getPredictionBets
+	} from '$lib/Blockchain/javascript/predictionsBlockchain';
+	import {
+		becomeMemberOfGroup,
+		removeGroupMembership,
+		isUserMemberInGroup,
+		getGroupsUserIsMemberIn
+	} from '$lib/Blockchain/javascript/rightToVote';
 
-   
-    
-    onMount(() => {
-        
-        // test(value); // Call test with group
-});
+	let userAddress = '';
+	let id = '';
+	let groupId = '';
+	let pollId = '';
+	let proposalId = '';
 
-    // function handleChange(event) {
-    //     value = event.target.valueAsNumber; // Converts input string to number
-    // }
+	async function connectWallet() {
+		if (typeof window.ethereum !== 'undefined') {
+			try {
+				const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+				userAddress = accounts[0];
+			} catch (error) {
+				console.error('Error connecting to MetaMask', error);
+			}
+		} else {
+			console.error('MetaMask is not installed!');
+		}
+	}
 
+	async function handleBecomeMember() {
+		try {
+			await becomeMemberOfGroup(groupId);
+		} catch (error) {
+			console.error('Error becoming member of group', error);
+		}
+	}
 
-    let userAddress = '';
+	async function handleRemoveMembership() {
+		try {
+			await removeGroupMembership();
+		} catch (error) {
+			console.error('Error removing membership from group', error);
+		}
+	}
 
-    async function connectWallet() {
+	async function handleCheckMembership() {
+		try {
+			const isMember = await isUserMemberInGroup();
+			console.log(`User is ${isMember ? '' : 'not '}a member of the group`);
+		} catch (error) {
+			console.error('Error checking membership', error);
+		}
+	}
 
-        if (typeof window.ethereum !== 'undefined') {
-            try {
-                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                userAddress = accounts[0];
-            } catch (error) {
-                console.error('Error connecting to MetaMask', error);
-            }
-        } else {
-            console.error('MetaMask is not installed!');
-        }
-    }
+	async function handleGetGroupsUserIsMemberIn() {
+		try {
+			const groups = await getGroupsUserIsMemberIn();
+			console.log('Groups user is member in:', groups);
+		} catch (error) {
+			console.error('Error getting groups user is member in', error);
+		}
+	}
+
+	async function handleCreatePoll() {
+		try {
+			await createPoll(groupId);
+		} catch (error) {
+			console.error('Error creating poll', error);
+		}
+	}
+
+	async function handleGetPoll() {
+		try {
+			const poll = await getPoll(id);
+			console.log('Poll:', poll);
+		} catch (error) {
+			console.error('Error getting poll', error);
+		}
+	}
+
+	async function handleCreateProposal() {
+		try {
+			await createProposal(pollId);
+		} catch (error) {
+			console.error('Error creating proposal', error);
+		}
+	}
+
+	async function handleGetProposalsOnPoll() {
+		try {
+			const proposals = await getProposalsOnPoll(pollId);
+			console.log('Proposals on poll:', proposals);
+		} catch (error) {
+			console.error('Error getting proposals on poll', error);
+		}
+	}
+
+	async function handleGetPollResults() {
+		try {
+			const results = await getPollResults(id);
+			console.log('Poll results:', results);
+		} catch (error) {
+			console.error('Error getting poll results', error);
+		}
+	}
+
+	async function handleVote() {
+		try {
+			await vote(pollId, proposalId);
+		} catch (error) {
+			console.error('Error voting', error);
+		}
+	}
+
+	async function handleCreatePrediction() {
+		try {
+			await createPrediction(id, 1);
+		} catch (error) {
+			console.error('Error creating prediction', error);
+		}
+	}
+
+	async function handleGetPredictionsOnPoll() {
+		try {
+			const predictions = await getPredictionsOnPoll(id, 1);
+			console.log('Predictions on poll:', predictions);
+		} catch (error) {
+			console.error('Error getting predictions on poll', error);
+		}
+	}
+
+	async function handleCreatePredictionBet() {
+		try {
+			await createPredictionBet(id, 1);
+		} catch (error) {
+			console.error('Error creating prediction bet', error);
+		}
+	}
+
+	async function handleGetPredictionBets() {
+		try {
+			const bets = await getPredictionBets(id, 1);
+			console.log('Prediction bets:', bets);
+		} catch (error) {
+			console.error('Error getting prediction bets', error);
+		}
+	}
 </script>
-<style>
-    hr {
-        border: 0;
-        border-top: 1px solid #8a8a8a;
-        margin: 20px 0;
-    }
-</style>
 
 <Layout>
-    <button on:click={connectWallet}>
-        {#if userAddress}
-            Connected: {userAddress}
-        {:else}
-            Connect Wallet
-        {/if}
-    </button>
+	<button on:click={connectWallet}>
+		{#if userAddress}
+			Connected: {userAddress}
+		{:else}
+			Connect Wallet
+		{/if}
+	</button>
 
-    
-    {#if userAddress}
-        <p>Wallet Address: {userAddress}</p>
-    {/if}
-    <hr>
-    <b>rightToVote</b>
-    <div class="p-6">
-        <button on:click={becomeMemberOfGroup}>Become member</button> 
-    </div> 
-    <div class="p-6">
-        <button on:click={removeGroupMembership}>Resign as member</button> 
-    </div> 
-    <div class="p-6">
-        <button on:click={isUserMemberInGroup}>Check if user is member of group</button> 
-    </div> 
-    <div class="p-6">
-        <button on:click={getGroupsUserIsMemberIn}>Get groups user is member in</button> 
-    </div> 
-    <hr>
-    <b>polls</b>
-    <div class="p-6">
-       
-        <button on:click={createPoll}>Create poll</button> 
-    </div>
-    <div class="p-6">
+	{#if userAddress}
+		<p>Wallet Address: {userAddress}</p>
+	{/if}
+	<hr />
+	<b>rightToVote</b>
+	<div class="p-6">
+		<input type="text" bind:value={groupId} placeholder="Enter Group ID">
+		<button on:click={handleBecomeMember}>Become member</button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleRemoveMembership}>Resign as member</button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleCheckMembership}>Check if user is member of group</button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleGetGroupsUserIsMemberIn}>Get groups user is member in</button>
+	</div>
+	<hr />
+	<b>polls</b>
+	<div class="p-6">
+		<input type="text" bind:value={groupId} placeholder="Enter Group ID">
+		<button on:click={handleCreatePoll}>Create poll</button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleGetPoll}>Get poll</button>
+	</div>
+	<div class="p-6">
+		<input type="text" bind:value={pollId} placeholder="Enter Poll Id">
+		<button on:click={handleCreateProposal}>Create proposal</button>
+	</div>
+	<div class="p-6">
+		<input type="text" bind:value={pollId} placeholder="Enter Poll Id">
+		<button on:click={handleGetProposalsOnPoll}>Get proposals </button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleGetPollResults}>Get poll results</button>
+	</div>
+	<div class="p-6">
+		<input type="text" bind:value={pollId} placeholder="Enter Poll Id">
+		<input type="text" bind:value={proposalId} placeholder="Enter Proposal Id">
+		<button on:click={handleVote}>Vote</button>
+	</div>
+	<hr />
+	<b>prediction/prediction bets</b>
+	<div class="p-6">
+		<button on:click={handleCreatePrediction}>Create prediction</button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleGetPredictionsOnPoll}>Get predictions on poll</button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleCreatePredictionBet}>Create prediction bet</button>
+	</div>
+	<div class="p-6">
+		<button on:click={handleGetPredictionBets}>Get prediction bets</button>
+	</div>
 
-        <button on:click={getPoll}>Get poll</button> 
-    </div>
-    <div class="p-6">
-
-        <button on:click={createProposal}>Create proposal</button> 
-    </div>
-    <div class="p-6">
-
-        <button on:click={getProposalsOnPoll}>Get proposals </button> 
-    </div>
-    <div class="p-6">
-
-        <button on:click={getPollResults}>Get pollresult</button> 
-    </div>
-    <div class="p-6">
-
-        <button on:click={vote}>vote</button> 
-    </div>
-    <hr>
-    <b>prediction/predictionbets</b>
-    <div class="p-6">
-
-        <button on:click={createPrediction}>Create prediction</button> 
-    </div>
-    <div class="p-6">
-
-        <button on:click={getPredictionsOnPoll}>Get predictions on poll</button> 
-    </div>
-    <div class="p-6">
-
-        <button on:click={createPredictionBet}>Create prediction bet</button> 
-    </div>
-    <div class="p-6">
-
-        <button on:click={getPredictionBets}>Get prediction bets</button> 
-    </div>
-    
-    
-    <hr>
-    <b>delegations</b>
+	<hr />
+	<b>delegations</b>
 </Layout>
 
+<style>
+	hr {
+		border: 0;
+		border-top: 1px solid #8a8a8a;
+		margin: 20px 0;
+	}
+</style>

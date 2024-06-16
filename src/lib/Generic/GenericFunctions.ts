8cdbbdf4cff2076537ee6a0421ec8d2ec4d69a31
Kattenelvis <officialkattenelvis@gmail.com>
2024-06-16 12:35:12 +0200

    Initializing new user info system with svelte stores

--- src/lib/Generic/GenericFunctions.ts
+++ src/lib/Generic/GenericFunctions.ts
@@ -1,3 +1,10 @@
+import { fetchRequest } from '$lib/FetchRequest';
+import type { GroupUser } from '$lib/Group/interface';
+import type { Permission } from '$lib/Group/Permissions/interface';
+import type { User } from '$lib/User/interfaces';
+import { writable } from 'svelte/store';
+import { page } from '$app/stores';
+
 //Hack to create a deep copy of an object
 export const deepCopy = (object: object) => {
 	return JSON.parse(JSON.stringify(object));
@@ -36,4 +43,22 @@ export const onThumbnailError = (event: any, picture: string) => {
 	if (!(event && event.target)) return;
 	event.target.src = picture;
 	event.onerror = null;
+};
+
+interface UserInfo {
+	user: User;
+	permission?: Permission;
+	groupuser?: GroupUser;
+}
+
+// User data is stored as the user navigates the site
+// When User enters a group, group user info and permissions about the user is taken in
+export let userInfo = writable<UserInfo>();
+
+export const getUserInfo = async () => {
+	const { res, json } = await fetchRequest(
+		'GET',
+		`user`
+	);
+	return json.results
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,14 @@
 	import '../app.css';
 	import { initializeLocalization } from '$lib/Localization/i18n';
 	import Header from '$lib/Header/Header.svelte';
+	import { onNavigate } from '$app/navigation';
 
 	initializeLocalization();
 	export const prerender = true;
+
+	onNavigate(() => {
+		console.log("Is this chat?")
+	})
 </script>
 
 <main class="h-[100wh]">

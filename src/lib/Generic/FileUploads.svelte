5cf2b6ccf7b6eb448fe7d634b1d0431eca2011d5
Kattenelvis <officialkattenelvis@gmail.com>
2024-06-29 13:10:27 +0200

    styling multiple media uploads

--- src/lib/Generic/FileUploads.svelte
+++ src/lib/Generic/FileUploads.svelte
@@ -7,30 +7,30 @@
 </script>
 
 <div>
-	<div class="flex justify-between items-center p-2 border dark:border-gray-500 border-gray-300">
-		Add media
-		<ImageUpload
-			icon={faUpload}
-			bind:croppedImage={image}
-			minimalist
-			label=""
-			iconSize="1.5x"
-			disableImagePreview
-			onCrop={() => {
-				if (image) {
-					images.push(image);
-					images = images;
-				}
-			}}
-		/>
-	</div>
-	<div class="flex flex-col gap-3 mt-3">
+	<div >
 		{#each images as image}
-			<div
-				class="flex justify-between items-center p-2 border dark:border-gray-500 border-gray-300"
-			>
-				{image.name}
-			</div>
-		{/each}
+		<div
+		class="flex justify-between items-center p-2 border dark:border-gray-500 border-gray-300"
+		>
+		{image.name}
 	</div>
+	{/each}
+</div>
+<div class="flex justify-between items-center p-2 border dark:border-gray-500 border-gray-300">
+	Add media
+	<ImageUpload
+		icon={faUpload}
+		bind:croppedImage={image}
+		minimalist
+		label=""
+		iconSize="1.5x"
+		disableImagePreview
+		onCrop={() => {
+			if (image) {
+				images.push(image);
+				images = images;
+			}
+		}}
+	/>
+</div>
 </div>

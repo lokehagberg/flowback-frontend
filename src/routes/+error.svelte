ac6830eb54c64d6d39aba3c9ae84ea88388db471
Kattenelvis <officialkattenelvis@gmail.com>
2024-07-07 13:43:17 +0200

    darkmode 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,8 @@
 <script>
-	import Layout from "$lib/Generic/Layout.svelte";
-
+	import Layout from '$lib/Generic/Layout.svelte';
 </script>
-<Layout >
 
-    <h1>404 not found</h1>
-<div class="text-center">No democracy found here</div>
-
+<Layout>
+	<h1 class="dark:text-darkmodeText">404 not found</h1>
+	<div class="text-center dark:text-darkmodeText">No democracy found here</div>
 </Layout>

2086321ba142f6e44fc19933eefdca9c5f6cf588
Kattenelvis <officialkattenelvis@gmail.com>
2024-07-23 22:33:43 +0200

    Description refactor

---
+++ src/lib/Poll/Description.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { poll } from "./interface";
+    import { page } from '$app/stores';
+
+    export let limit:number, poll:poll, onHoverGroup:boolean;
+
+    let readMore = false
+</script>
+
+<p class="mt-2 whitespace-pre-wrap break-words mb-4 dark:text-darkmodeText">
+    {#if readMore}
+        <a
+            class="cursor-pointe text-black hover:underline"
+            href={onHoverGroup
+                ? '/groups/1'
+                : `/groups/${poll.group_id || $page.params.groupId}/polls/${poll.id}`}
+        >
+            {poll.description}
+        </a>
+        {#if poll.description.length > limit}
+            <!-- svelte-ignore a11y-no-static-element-interactions -->
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <span
+                class="hover:underline cursor-pointer text-gray-600 font-bold ml-3"
+                on:click={() => (readMore = false)}>Show less...</span
+            >
+        {/if}
+    {:else}
+        <a
+            class="cursor-pointe text-black hover:underline"
+            href={onHoverGroup
+                ? '/groups/1'
+                : `/groups/${poll.group_id || $page.params.groupId}/polls/${poll.id}`}
+        >
+            {poll.description.slice(0, limit)}
+        </a>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        {#if poll.description.length > limit}
+            <span
+                class="hover:underline cursor-pointer text-gray-600 font-bold ml-3"
+                on:click={() => (readMore = true)}>Show more...</span
+            >
+        {/if}
+    {/if}
+</p>
--- src/lib/Poll/PollThumbnail.svelte
+++ src/lib/Poll/PollThumbnail.svelte
@@ -24,6 +24,7 @@
 	import { env } from '$env/dynamic/public';
 	import { darkModeStore } from '$lib/Generic/DarkMode';
 	import Button from '$lib/Generic/Button.svelte';
+	import Description from './Description.svelte';
 
 	export let poll: poll,
 		isAdmin = false;
@@ -171,43 +172,7 @@
 		<div class="text-sm">{$_('Current phase:')} {getPhaseUserFriendlyName(phase)}</div>
 	</div>
 
-	<p class="mt-2 whitespace-pre-wrap break-words mb-4 dark:text-darkmodeText">
-		{#if readMore}
-			<a
-				class="cursor-pointe text-black hover:underline"
-				href={onHoverGroup
-					? '/groups/1'
-					: `/groups/${poll.group_id || $page.params.groupId}/polls/${poll.id}`}
-			>
-				{poll.description}
-			</a>
-			{#if poll.description.length > 500}
-				<!-- svelte-ignore a11y-no-static-element-interactions -->
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<span
-					class="hover:underline cursor-pointer text-gray-600 font-bold ml-3"
-					on:click={() => (readMore = false)}>Show less...</span
-				>
-			{/if}
-		{:else}
-			<a
-				class="cursor-pointe text-black hover:underline"
-				href={onHoverGroup
-					? '/groups/1'
-					: `/groups/${poll.group_id || $page.params.groupId}/polls/${poll.id}`}
-			>
-				{poll.description.slice(0, 500)}
-			</a>
-			<!-- svelte-ignore a11y-no-static-element-interactions -->
-			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			{#if poll.description.length > 500}
-				<span
-					class="hover:underline cursor-pointer text-gray-600 font-bold ml-3"
-					on:click={() => (readMore = true)}>Show more...</span
-				>
-			{/if}
-		{/if}
-	</p>
+	<Description limit={500} {poll} {onHoverGroup} />
 
 	<Timeline displayDetails={false} pollType={poll.poll_type} bind:dates />
 

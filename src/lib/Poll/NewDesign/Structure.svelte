f25fa2b86067bb59c7caab95e13beb88e156f614
Kattenelvis <officialkattenelvis@gmail.com>
2024-07-28 20:31:49 +0200

    Grid works

--- src/lib/Poll/NewDesign/Structure.svelte
+++ src/lib/Poll/NewDesign/Structure.svelte
@@ -1,47 +1,45 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import type { poll } from "../interface";
-	import Timeline from "./Timeline.svelte";
+	import { onMount } from 'svelte';
+	import type { poll } from '../interface';
+	import Timeline from './Timeline.svelte';
 
-    export let poll:poll;
+	export let poll: poll;
 
 	let genericStyle = 'bg-white p-6';
-    onMount(() => {
-        console.log(poll)
-    })
-    $: console.log(poll);
-    
+	onMount(() => {
+		console.log(poll);
+	});
+	$: console.log(poll);
 </script>
 
-
 {#if poll}
-<div class="poll-grid p-12">
-	
-	<Timeline displayDetails={false} bind:poll pollType={4} Class="timeline-grid" />
+	<div class="poll-grid p-12">
+		<Timeline displayDetails={false} bind:poll pollType={4} Class="timeline-grid" />
 
-	{#if $$slots.left}
-		<div class={`${genericStyle}`}>
-			<slot name="left" />
-		</div>
-	{/if}
+		{#if $$slots.left}
+			<div class={`${genericStyle}`}>
+				<slot name="left" />
+			</div>
+		{/if}
 
-	{#if $$slots.right}
-		<div class={`${genericStyle}`}>
-			<slot name="right" />
-		</div>
-	{/if}
+		{#if $$slots.right}
+			<div class={`${genericStyle}`}>
+				<slot name="right" />
+			</div>
+		{/if}
 
-	{#if $$slots.bottom}
-		<div class={`${genericStyle} bottom-grid`}>
-			<slot name="bottom" />
-		</div>
-	{/if}
-</div>
+		{#if $$slots.bottom}
+			<div class={`${genericStyle} bottom-grid`}>
+				<slot name="bottom" />
+			</div>
+		{/if}
+	</div>
+{/if}
 
 <style>
 	.poll-grid {
 		grid-template-columns: 0.2fr 1fr 1fr;
-		grid-template-rows: 60% 40%;
+		grid-template-rows: 50vh 40vh;
 		display: grid;
 		gap: 20px;
 	}
@@ -54,4 +52,3 @@
 		grid-area: 2 / 2 / 2 / 4;
 	}
 </style>
-{/if}

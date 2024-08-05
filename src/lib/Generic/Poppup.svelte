8f7c5a25c51966f05b298b230e16321fdb1fd335
Kattenelvis <officialkattenelvis@gmail.com>
2024-08-05 12:25:50 +0200

    poppup fixes

--- src/lib/Generic/Poppup.svelte
+++ src/lib/Generic/Poppup.svelte
@@ -6,31 +6,30 @@
 
 	export let poppup: poppup | null;
 
-	let show = true,
+	let show = false,
 		currentPoppup: NodeJS.Timeout;
 
 	$: poppup && successPoppupAnimation();
 
 	const successPoppupAnimation = () => {
 		show = true;
-		// clearTimeout(currentPoppup);
-		// currentPoppup = setTimeout(() => {
-		// 	show = false;
-		// 	poppup = null;
-		// }, 7000);
+		clearTimeout(currentPoppup);
+		currentPoppup = setTimeout(() => {
+			show = false;
+			poppup = null;
+		}, 7000);
 	};
 </script>
 
 {#if poppup?.message}
-	<div class="bg-white fixed bottom-5 right-5 z-50" hidden={!show}>
-		<div
-			id="popup"
-			class={`flex gap-2 items-center px-4 py-2 rounded-md shadow-md opacity-0 transition-opacity duration-300 transform translate-y-3`}
-			class:show
-		>
-			<Fa icon={faCheckCircle} color={poppup.success ? 'green' : 'red'} />
-			{$_(poppup.message)}
-		</div>
+	<div
+		id="popup"
+		class={`bg-white fixed bottom-5 right-5 z-50 shadow-xl flex gap-2 items-center  px-4 py-2 rounded-md opacity-0 transition-opacity duration-300 transform translate-y-3`}
+		class:show
+		hidden={!show}
+	>
+		<Fa icon={faCheckCircle} color={poppup.success ? 'green' : 'red'} />
+		{$_(poppup.message)}
 	</div>
 {/if}
 

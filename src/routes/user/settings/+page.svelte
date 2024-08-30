622de83f54d25581847a6998f60e41365221abbc
Kattenelvis <officialkattenelvis@gmail.com>
2024-08-30 12:42:48 +0200

    settings design

--- src/routes/user/settings/+page.svelte
+++ src/routes/user/settings/+page.svelte
@@ -3,7 +3,8 @@
 	import Fa from 'svelte-fa';
 	import { faUser, faBell, faPieChart } from '@fortawesome/free-solid-svg-icons';
 
-	let selectedPage: 'profile' | 'notifications' | 'poll-process' = 'profile';
+	let selectedPage: 'profile' | 'notifications' | 'poll-process' = 'profile',
+		optionsDesign = 'flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-1 ';
 </script>
 
 <Layout centered>
@@ -13,29 +14,42 @@
 			<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<ul>
-				<li on:click={() => (selectedPage = 'profile')} class="flex items-center gap-2">
+				<li
+					on:click={() => (selectedPage = 'profile')}
+					class={`${optionsDesign}`}
+					class:border-l-2={selectedPage === 'profile'}
+					class:border-primary={selectedPage === 'profile'}
+				>
 					<Fa icon={faUser} />Profile
 				</li>
-				<li on:click={() => (selectedPage = 'notifications')} class="flex items-center gap-2">
+				<li
+					on:click={() => (selectedPage = 'notifications')}
+					class={`${optionsDesign}`}
+					class:border-l-2={selectedPage === 'notifications'}
+					class:border-primary={selectedPage === 'notifications'}
+				>
 					<Fa icon={faBell} />Notification
 				</li>
-				<li on:click={() => (selectedPage = 'poll-process')} class="flex items-center gap-2">
+				<li
+					on:click={() => (selectedPage = 'poll-process')}
+					class={`${optionsDesign}`}
+					class:border-l-2={selectedPage === 'poll-process'}
+					class:border-primary={selectedPage === 'poll-process'}
+				>
 					<Fa icon={faPieChart} />Poll Process
 				</li>
 			</ul>
 		</div>
 		<div class="bg-white p-6 w-[400px]">
-            <ul>
-                {#if selectedPage === 'profile'}
-                <li>one</li>
-                {:else if selectedPage === 'notifications'}
-                <li>two</li>
-                {:else if selectedPage === 'poll-process'}
-                <li>three</li>
-                {/if}
-
-            </ul>
-            
-            </div>
+			<ul>
+				{#if selectedPage === 'profile'}
+					<li>one</li>
+				{:else if selectedPage === 'notifications'}
+					<li>two</li>
+				{:else if selectedPage === 'poll-process'}
+					<li>three</li>
+				{/if}
+			</ul>
+		</div>
 	</div>
 </Layout>

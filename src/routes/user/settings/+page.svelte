78ca32b40e8b5b47cd629de350beb5131677e128
Kattenelvis <officialkattenelvis@gmail.com>
2024-08-31 11:55:31 +0200

    settings work

--- src/routes/user/settings/+page.svelte
+++ src/routes/user/settings/+page.svelte
@@ -3,8 +3,20 @@
 	import Fa from 'svelte-fa';
 	import { faUser, faBell, faPieChart } from '@fortawesome/free-solid-svg-icons';
 
-	let selectedPage: 'profile' | 'notifications' | 'poll-process' = 'profile',
-		optionsDesign = 'flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-1 ';
+	let selectedPage: 'profile' | 'notifications' | 'poll-process' = 'notifications',
+		optionsDesign = 'flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-1 transition-all',
+		notificationSettingsTitles = ['Schedule', 'Kanban', 'Posts'],
+		notificationSettings = [
+			[
+				'I am invited to an event',
+				"an event's date has been changed",
+				'an event has been canceled',
+				'a new member has been added',
+				"an event's frequency has been changed"
+			],
+			['a task is assigned to you', "a task's priority is changed", "a task's status is changed"],
+			['a new thread is created', 'a new poll is created', 'someone votes on my comment']
+		];
 </script>
 
 <Layout centered>
@@ -13,7 +25,7 @@
 			<h1 class="text-xl text-left text-primary font-bold">Settings</h1>
 			<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<ul>
+			<ul class="mt-4">
 				<li
 					on:click={() => (selectedPage = 'profile')}
 					class={`${optionsDesign}`}
@@ -41,11 +53,24 @@
 			</ul>
 		</div>
 		<div class="bg-white p-6 w-[400px]">
-			<ul>
+			Notify me when...
+			<ul class="flex flex-col">
 				{#if selectedPage === 'profile'}
 					<li>one</li>
 				{:else if selectedPage === 'notifications'}
-					<li>two</li>
+					{#each notificationSettingsTitles as title, i}
+						<li>
+							<span class="text-xl text-primary font-bold">{title}</span>
+							<ul>
+								{#each notificationSettings[i] as setting}
+									<li class="flex justify-between">
+										<span>{setting}</span>
+										<input type="checkbox" />
+									</li>
+								{/each}
+							</ul>
+						</li>
+					{/each}
 				{:else if selectedPage === 'poll-process'}
 					<li>three</li>
 				{/if}

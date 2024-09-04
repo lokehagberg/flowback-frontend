da0f2da3a2729e63199b792b214ffdf948abdef0
Kattenelvis <officialkattenelvis@gmail.com>
2024-09-04 14:36:31 +0200

    proposal creation date poll on grid

--- src/lib/Generic/Grid.svelte
+++ src/lib/Generic/Grid.svelte
@@ -3,6 +3,8 @@
 	import Fa from 'svelte-fa';
 	import { getMonday, getWeek } from './Dates';
 	import { onMount } from 'svelte';
+	import { fetchRequest } from '$lib/FetchRequest';
+	import { page } from '$app/stores';
 
 	export let x = 10,
 		y = 10;
@@ -33,10 +35,51 @@
 			selectedDates = selectedDates.filter((_date) => _date?.getTime() !== date?.getTime());
 		else selectedDates.push(date);
 
-		selectedDates = selectedDates
+		selectedDates = selectedDates;
 	};
 
-	$: monday = getRecentMonday(new Date(initialMonday?.getFullYear(), initialMonday?.getMonth(), initialMonday?.getDate() - weekOffset*7))
+	const saveSelection = async () => {
+		const array = selectedDates.map(async (date) => {
+			const start_date = date;
+			const end_date = new Date(
+				date.getFullYear(),
+				date.getMonth(),
+				date.getDate(),
+				date.getHours() + 1
+			);
+
+			try {
+				const { res, json } = await fetchRequest(
+					'POST',
+					`group/poll/${$page.params.pollId}/proposal/create`,
+					{
+						start_date,
+						end_date
+					}
+				);
+			} catch {
+				const { res, json } = await fetchRequest(
+					'POST',
+					`group/poll/${$page.params.pollId}/proposal/vote/update`,
+					{
+						start_date,
+						end_date
+					}
+				);
+			}
+		});
+
+		let output = await Promise.allSettled(array);
+		console.log(output);
+	};
+
+	$: monday = getRecentMonday(
+		new Date(
+			initialMonday?.getFullYear(),
+			initialMonday?.getMonth(),
+			initialMonday?.getDate() + weekOffset * 7
+		)
+	);
 
 	$: gridDates = Array.from({ length: y }, (_, j) =>
 		Array.from(
@@ -50,6 +93,7 @@
 <button on:click={() => year--}>year down</button>
 <button on:click={() => weekOffset++}>week up</button>
 <button on:click={() => weekOffset--}>week down</button>
+<button on:click={saveSelection}>le submit</button>
 
 {year}
 {weekOffset}
@@ -63,14 +107,14 @@
 		>
 			{#each gridDates as row, j}
 				{#each row as date, i}
-			
 					<div
 						class="border p-4"
 						on:click={() => toggleDate(date)}
 						on:keydown
 						role="button"
 						tabindex="0"
-					>	{date}
+					>
+						{date}
 						{#if selectedDates.find((_date) => _date?.getTime() === date?.getTime())}
 							<div class="bg-green-600 p-6"><Fa icon={faCheck} color="white" /></div>
 						{:else}

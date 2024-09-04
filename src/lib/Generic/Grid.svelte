d37148e562e45ce84a306534723a6e8ba6d5e078
Kattenelvis <officialkattenelvis@gmail.com>
2024-09-04 10:18:24 +0200

    week schedule work

--- src/lib/Generic/Grid.svelte
+++ src/lib/Generic/Grid.svelte
@@ -1,38 +1,80 @@
 <script lang="ts">
 	import { faCheck } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
+	import { getMonday, getWeek } from './Dates';
+	import { onMount } from 'svelte';
 
 	export let x = 10,
 		y = 10;
 	// w = 200,
 	// h = 300;
 
-	let clicked = [[1, 1]];
+	let clicked = [[1, 1]],
+		selectedDates: Date[] = [],
+		week: number = getWeek(new Date()),
+		year: number = new Date().getFullYear(),
+		monday: Date;
+
+	onMount(() => {
+		monday = getRecentMonday(new Date());
+	});
+
+	const getRecentMonday = (d: Date) => {
+		let mondayOffset = d.getDate() - d.getDay() + 1;
+		let month = mondayOffset < 0 ? d.getMonth() - 1 : d.getMonth();
+
+		return new Date(d.getFullYear(), month, mondayOffset);
+	};
 </script>
 
-<div
-	class="grid"
-	style={`grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${y}, 1fr);`}
->
-	{#each { length: y } as _, j}
-		{#each { length: x } as _, i}
-			<div
-				class="border p-4"
-				on:click={() => {
-					clicked.filter((pos) => pos[0] === i && pos[1] === j);
-					clicked.push([i, j]);
-                    clicked = clicked
-				}}
-				on:keydown
-				role="button"
-				tabindex="0"
-			>
-				{#if clicked.find((pos) => pos[0] === i && pos[1] === j)}
-					<div class="bg-green-600 p-6"><Fa icon={faCheck} color="white" /></div>
-				{:else}
-					<slot {i} {j} />
-				{/if}
-			</div>
+<button on:click={() => year++}>year up</button>
+<button on:click={() => year--}>year down</button>
+<button on:click={() => week++}>week up</button>
+<button on:click={() => week--}>week down</button>
+
+{year}
+{week}
+
+{selectedDates.length}
+
+{#if monday}
+	<div
+		class="grid"
+		style={`grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${y}, 1fr);`}
+	>
+		{#each { length: y } as _, j}
+			{#each { length: x } as _, i}
+				{@const date = new Date(monday.getFullYear(), monday.getMonth(), monday.getDate() + i, j)}
+				<div
+					class="border p-4"
+					on:click={() => {
+						// clicked.push([i, j]);
+						// clicked = clicked;
+						selectedDates.push(date);
+						selectedDates = selectedDates;
+						console.log(
+							date,
+							selectedDates,
+							'CAN YOU HERE THE BEAT?',
+							selectedDates.find((_date) => _date === date)
+						);
+					}}
+					on:keydown
+					role="button"
+					tabindex="0"
+				>
+					{date}
+					{selectedDates}
+					{#key selectedDates}
+					{selectedDates.findIndex((_date) => _date === date)}
+						{#if selectedDates.find((_date) => _date === date)}zzz
+							<div class="bg-green-600 p-6"><Fa icon={faCheck} color="white" /></div>
+						{:else}
+							<slot {i} {j} />
+						{/if}
+					{/key}
+				</div>
+			{/each}
 		{/each}
-	{/each}
-</div>
+	</div>
+{/if}

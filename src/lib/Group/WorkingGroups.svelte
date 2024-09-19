b52a86473bf0e9f9f8a3a9a5375ead41b8d7c37d
Kattenelvis <officialkattenelvis@gmail.com>
2024-09-19 12:45:19 +0200

    working groups designed

--- src/lib/Group/WorkingGroups.svelte
+++ src/lib/Group/WorkingGroups.svelte
@@ -11,16 +11,17 @@
 </script>
 
 {#each workingGroups as workingGroup}
-	<div class="bg-white dark:bg-darkobject w-full p-6 mb-6">
-		{workingGroup.name}
-		{workingGroup.members}
+	<div class="bg-white dark:bg-darkobject w-full p-6 mb-6 flex justify-between items-center">
+		
+        <span class="text-primary w-[20%]">{workingGroup.name}</span>
+		<span class="text-gray-500 text-sm">Members: {workingGroup.members} </span>
 
 		{#if workingGroup.isMember}
-			<Button buttonStyle="warning">Leave</Button>
+			<Button buttonStyle="warning" Class="px-3 py-1 w-[20%]">Leave</Button>
 		{:else if workingGroup.invite}
-			<Button buttonStyle="primary-light">Ask to Join</Button>
+			<Button buttonStyle="primary-light" Class="px-3 py-1 w-[20%]">Ask to Join</Button>
 		{:else}
-			<Button buttonStyle="primary-light">Join</Button>
+			<Button buttonStyle="primary-light" Class="px-3 py-1 w-[20%]">Join</Button>
 		{/if}
 	</div>
 {/each}

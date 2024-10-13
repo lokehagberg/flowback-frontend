b81a5752309673cec6889cfb9b9546cb505aea06
mashavaverova <129207381+mashavaverova@users.noreply.github.com>
2024-10-14 00:30:14 +0200

    Upd CodeBase and  UnitTests
    second big upd contract code and unit tests (comments and documentation need to be updated)

--- src/lib/Blockchain/smart-contracts/PredictionBetHelpers.sol
+++ src/lib/Blockchain/smart-contracts/PredictionBetHelpers.sol
@@ -1,18 +1,34 @@
-   // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
-import {PollHelpers} from './PollHelpers.sol';
-import {ProposalHelpers} from './ProposalHelpers.sol'; 
-import {PredictionHelpers} from './PredictionHelpers.sol'; 
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.18;
 
+import {PollHelpers} from "./PollHelpers.sol";
+import {ProposalHelpers} from "./ProposalHelpers.sol";
+import {PredictionHelpers} from "./PredictionHelpers.sol";
+
+/**
+ * @title PredictionBetHelpers
+ * @dev This contract extends PollHelpers, ProposalHelpers, and PredictionHelpers to manage prediction bets related to polls and proposals.
+ * @notice This contract facilitates the storage and management of prediction bets made by users on proposals and predictions within polls.
+ * @author @EllenLng, @KristofferGW
+ * @notice Audited by @MashaVaverova
+ */
 contract PredictionBetHelpers is PollHelpers, ProposalHelpers, PredictionHelpers {
-   
-     struct PredictionBet{
-        uint pollId;
-        uint proposalId;
-        uint predictionId;
+    /**
+     * @dev Struct to represent a prediction bet placed by a user.
+     * @param pollId The ID of the poll where the bet is placed.
+     * @param proposalId The ID of the proposal within the poll that the bet is related to.
+     * @param predictionId The ID of the prediction related to the bet.
+     * @param bet A boolean representing whether the bet is for or against the prediction.
+     * @param likelihood The likelihood score assigned to the prediction.
+     */
+    struct PredictionBet {
+        uint256 pollId;
+        uint256 proposalId;
+        uint256 predictionId;
         bool bet;
-        uint likelihood;
+        uint256 likelihood;
     }
 
-    mapping(uint => PredictionBet[]) public predictionBets;
+    /// @notice Maps a poll ID to an array of PredictionBet structures representing all bets made in that poll.
+    mapping(uint256 => mapping(uint256 => PredictionBet[])) public predictionBets;
 }
---
+++ src/lib/Blockchain/test/RightToVote.t.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import "forge-std/Test.sol";
+import "src/Polls.sol";
+import "forge-std/Vm.sol";
+
+contract RightToVoteTest is Test, Polls {
+    Polls public testPolls;
+    address user1 = address(0x1);
+    address user2 = address(0x2);
+
+    function setUp() public {
+        testPolls = new Polls();
+    }
+
+    function run() public {
+        vm.broadcast();
+    }
+
+    function testbecomeMemberOfGroup(uint256 group) public {
+        vm.startPrank(user1);
+        assertFalse(testPolls.isUserMemberOfGroup(group));
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(group);
+        assertTrue(testPolls.isUserMemberOfGroup(group));
+        assertEq(testPolls.getGroupsUserIsMemberIn()[0], group);
+    }
+
+    function testRemoveGroupMembership(uint256 group) public {
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(group);
+        assertTrue(testPolls.isUserMemberOfGroup(group));
+        vm.startPrank(user1);
+        testPolls.removeGroupMembership(group);
+        assertEq(testPolls.isUserMemberOfGroup(group), false);
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(1);
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(2);
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(3);
+        uint256[] memory groupsOne = testPolls.getGroupsUserIsMemberIn();
+        assertEq(groupsOne.length, 3);
+        vm.startPrank(user1);
+        testPolls.removeGroupMembership(2);
+        uint256[] memory groups = testPolls.getGroupsUserIsMemberIn();
+        assertEq(testPolls.getGroupsUserIsMemberIn()[0], 1);
+        assertEq(testPolls.getGroupsUserIsMemberIn()[1], 3);
+        assertEq(groups.length, 2);
+    }
+
+    function testGetGroupsUserIsMemberIn() public {
+        uint256[] memory groupsOne = testPolls.getGroupsUserIsMemberIn();
+        assertEq(groupsOne.length, 0);
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(1);
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(4);
+        uint256[] memory groups = testPolls.getGroupsUserIsMemberIn();
+        assertTrue(groups.length > 0);
+        assertEq(groups.length, 2);
+        assertEq(testPolls.getGroupsUserIsMemberIn()[0], 1);
+        assertEq(testPolls.getGroupsUserIsMemberIn()[1], 4);
+        vm.startPrank(user1);
+        testPolls.becomeMemberOfGroup(3);
+        assertEq(testPolls.getGroupsUserIsMemberIn()[2], 3);
+    }
+}

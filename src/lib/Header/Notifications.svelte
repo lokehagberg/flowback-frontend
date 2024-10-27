34fe0d8ee05f750e1484570bc118d8437f16c9f5
Kattenelvis <officialkattenelvis@gmail.com>
2024-10-27 14:28:13 +0100

    buttons

--- src/lib/Header/Notifications.svelte
+++ src/lib/Header/Notifications.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Fa from 'svelte-fa';
+	//@ts-ignore
+	import Fa from 'svelte-fa/src/fa.svelte';
 	import { faBell } from '@fortawesome/free-solid-svg-icons/faBell';
 	import { _ } from 'svelte-i18n';
 	import { fetchRequest } from '$lib/FetchRequest';
@@ -86,23 +87,20 @@
 		class="absolute right-0 top-full bg-white dark:bg-darkobject dark:text-darkmodeText select-none shadow slide-animation z-[60]"
 		id="notifications-list"
 	>
-		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-		<li
+		<button
 			on:click={markAllAsRead}
 			on:keydown
 			class="flex justify-end items-center cursor-pointer border-b border-gray-200 border hover:shadow hover:bg-blue-300 transition-all"
 		>
 			{$_('Mark all as read')}
-		</li>
+		</button>
 		{#if notifications?.length > 0}
 			{#each notifications as notification}
-				<!-- svelte-ignore a11y-no-static-element-interactions -->
 				<li
 					class="flex justify-end items-center cursor-pointer border-b border-gray-200 border hover:shadow hover:bg-blue-300 transition-all"
 					class:bg-gray-200={hovered.find((hover) => hover === notification.id)}
 				>
-					<!-- svelte-ignore a11y-no-static-element-interactions -->
-					<div
+					<button
 						class="pt-3 pb-3 pr-10 pl-6"
 						on:click={async () => {
 							if (notification.channel_sender_type === 'group')
@@ -116,14 +114,13 @@
 								goto(`/groups/${groupId}/polls/${notification.channel_sender_id}`);
 							}
 						}}
-						on:keydown
 					>
 						<div class=" max-w-[25vw] overflow-x-hidden overflow-ellipsis whitespace-nowrap">
 							{$_(notification.message)}
 							{timeAgo.format(new Date(notification.timestamp))}
 						</div>
-					</div>
-					<div
+					</button>
+					<button
 						style="z-index: 1;"
 						class="mr-8"
 						on:click={() => {
@@ -134,10 +131,9 @@
 							// hovered.push(notification.id);
 							// hovered = hovered;
 						}}
-						on:keydown
 					>
 						<Fa icon={faX} />
-					</div>
+					</button>
 				</li>
 			{/each}
 		{:else}

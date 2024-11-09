ffb19e7aca9aa396456105c830e39c198a2d7cd2
kattenelvis <officialkattenelvis@gmail.com>
2024-11-09 12:35:41 +0100

    woaw

--- src/lib/Group/GroupSidebarButton.svelte
+++ src/lib/Group/GroupSidebarButton.svelte
@@ -7,7 +7,7 @@
 		action = () => {},
 		icon = faPoll,
 		isSelected = true,
-		Class = ""
+		Class = '';
 
 	let hovering = false;
 </script>
@@ -19,7 +19,9 @@
 	on:click={action}
 	class={`${Class} select-none flex item-center items-center  dark:outline-gray-700 rounded-md transition-all  ${
 		!isSelected && 'hover:bg-gray-200 active:bg-gray-300  '
-	} outline-1 cursor-pointer p-4 ${isSelected && 'text-gray-400 dark:text-gray-500'} `}
+	} outline-1 cursor-pointer p-3 ${
+		isSelected && 'text-gray-400 dark:text-gray-500 border-l-2 border-primary'
+	} `}
 >
 	<Fa {icon} />
 	<div class="ml-2">

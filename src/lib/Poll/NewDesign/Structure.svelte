880c3865e7b6394f44dde75d6c931becb52a74f6
kattenelvis <officialkattenelvis@gmail.com>
2024-11-20 01:05:53 +0100

    small fix

--- src/lib/Poll/NewDesign/Structure.svelte
+++ src/lib/Poll/NewDesign/Structure.svelte
@@ -6,7 +6,7 @@
 	export let poll: poll,
 		Class = '';
 
-	let genericStyle = 'bg-white dark:bg-darkobject dark:text-darkmodeText p-6 h-[100%] h-full';
+	let genericStyle = 'bg-white dark:bg-darkobject dark:text-darkmodeText p-6 h-[100%] h-full overflow-y-scroll';
 
 	onMount(() => {});
 </script>

a86e08e18d02b16a9e3b80332f2943237c306810
kattenelvis <officialkattenelvis@gmail.com>
2024-12-01 16:07:12 +0100

    admin settings design

--- src/routes/groups/[groupId]/edit/+page.svelte
+++ src/routes/groups/[groupId]/edit/+page.svelte
@@ -9,28 +9,60 @@
 	import Fa from 'svelte-fa';
 	import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-	let selectedPage: 'group' | 'areas' | 'perms' | 'blockchain' | null = null;
+	let selectedPage: 'group' | 'areas' | 'perms' | 'blockchain' = 'group';
 </script>
 
-<button
-	class="cursor-pointer bg-white dark:bg-darkobject dark:text-darkmodeText justify-center m-auto"
-	on:click={() => goto(`/groups/${$page.params.groupId}`)}
->
-	<!-- NOTE: In +layout, rote folder, there are URL related behaviours which are affected by this. -->
-	<Fa icon={faArrowLeft} />
-</button>
 <Layout centered>
-	<div class="flex w-[80%] mt-6 gap-6">
-		<div class="bg-white p-6 shadow">
+	<div class="flex w-full md:w-[80%] max-w-[1200px] mt-6 md:gap-6 relative">
+		<button
+			class="cursor-pointer dark:bg-darkobject dark:text-darkmodeText absolute top-0"
+			on:click={() => goto(`/groups/${$page.params.groupId}`)}
+		>
+			<!-- NOTE: In +layout, rote folder, there are URL related behaviours which are affected by this. -->
+			<Fa icon={faArrowLeft} />
+		</button>
+		<div class="bg-white rounded p-6 shadow ml-6 w-[30%]">
 			<h1 class="text-xl font-semibold text-primary text-left">{$_('Admin Settings')}</h1>
 			<ul>
-				<li><button on:click={() => (selectedPage = 'group')}>{$_('Edit Group')}</button></li>
-				<li><button on:click={() => (selectedPage = 'areas')}>{$_('Areas')}</button></li>
-				<li><button on:click={() => (selectedPage = 'perms')}>{$_('Permissions')}</button></li>
-				<li><button on:click={() => (selectedPage = 'blockchain')}>{$_('Blockhain')}</button></li>
+				<li
+					class={`cursor-pointer ${
+						selectedPage === 'group' && 'bg-gray-100 border-l-2 border-primary transition-all'
+					}`}
+				>
+					<button class="w-full text-left" on:click={() => (selectedPage = 'group')}
+						>{$_('Edit Group')}</button
+					>
+				</li>
+				<li
+					class={`cursor-pointer ${
+						selectedPage === 'areas' && 'bg-gray-100 border-l-2 border-primary transition-all'
+					}`}
+				>
+					<button class="w-full text-left" on:click={() => (selectedPage = 'areas')}
+						>{$_('Areas')}</button
+					>
+				</li>
+				<li
+					class={`cursor-pointer ${
+						selectedPage === 'perms' && 'bg-gray-100 border-l-2 border-primary transition-all'
+					}`}
+				>
+					<button class="w-full text-left" on:click={() => (selectedPage = 'perms')}
+						>{$_('Permissions')}</button
+					>
+				</li>
+				<li
+					class={`cursor-pointer ${
+						selectedPage === 'blockchain' && 'bg-gray-100 border-l-2 border-primary transition-all'
+					}`}
+				>
+					<button class="w-full text-left" on:click={() => (selectedPage = 'blockchain')}
+						>{$_('Blockhain')}</button
+					>
+				</li>
 			</ul>
 		</div>
-		<div class="bg-white p-6 shadow flex-grow">
+		<div class="bg-white p-6 shadow flex-grow rounded">
 			{#if selectedPage === 'group'}
 				<EditGroup />
 			{:else if selectedPage === 'areas'}

b8944584ab0f63744a90853f4119b18df6559abb
kattenelvis <officialkattenelvis@gmail.com>
2025-02-02 16:06:34 +0100

    a

--- src/lib/Chat/Preview.svelte
+++ src/lib/Chat/Preview.svelte
@@ -172,7 +172,7 @@
 				profilePicture={chatter.profile_image}
 			/>
 			<div class="flex flex-col">
-				<span class="max-w-[12vw] overflow-x-hidden overflow-ellipsis"
+				<span class="max-w-[12vw] text-left overflow-x-hidden overflow-ellipsis"
 					>{chatter.name || chatter.username}</span
 				>
 				<span class="text-gray-400 text-sm truncate h-[20px] overflow-x-hidden max-w-[10vw]">

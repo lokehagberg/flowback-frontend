398074d30d3c0a014133adad14bb3c39fb15ce3c
kattenelvis <officialkattenelvis@gmail.com>
2025-02-02 19:56:39 +0100

    Restrict voting buttons to non-authors in Comment component

--- src/lib/Comments/Comment.svelte
+++ src/lib/Comments/Comment.svelte
@@ -143,21 +143,22 @@
 				>
 					<Fa icon={faReply} />{$_('Reply')}
 				</button>
-				<button
-					class:text-primary={comment.user_vote === true}
-					class="flex items-center gap-1 cursor-pointer transition-colors"
-					on:click={() => commentVote(1)}
-				>
-					<Fa icon={faArrowUp} />
-				</button>
-				<button
-					class:text-primary={comment.user_vote === false}
-					class="flex items-center gap-1 cursor-pointer transition-colors"
-					on:click={() => commentVote(-1)}
-				>
-					<Fa icon={faArrowDown} />
-				</button>
-
+				{#if comment.author_id !== Number(localStorage.getItem('userId'))}
+					<button
+						class:text-primary={comment.user_vote === true}
+						class="flex items-center gap-1 cursor-pointer transition-colors"
+						on:click={() => commentVote(1)}
+					>
+						<Fa icon={faArrowUp} />
+					</button>
+					<button
+						class:text-primary={comment.user_vote === false}
+						class="flex items-center gap-1 cursor-pointer transition-colors"
+						on:click={() => commentVote(-1)}
+					>
+						<Fa icon={faArrowDown} />
+					</button>
+				{/if}
 				{comment.score}
 
 				{#if Number(localStorage.getItem('userId')) === comment.author_id}

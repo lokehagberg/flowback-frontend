475957c9a9aca2d4c9f7b16c3242e667b2b2529c
kattenelvis <officialkattenelvis@gmail.com>
2025-03-20 13:57:06 +0100

    Built out createchatgroup component

--- src/lib/Chat/CreateGroup.svelte
+++ src/lib/Chat/CreateGroup.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
+	import { fetchRequest } from '$lib/FetchRequest';
+	import Button from '$lib/Generic/Button.svelte';
 	import TextInput from '$lib/Generic/TextInput.svelte';
+	import { _ } from 'svelte-i18n';
 
-	export let creatingGroup: boolean;
-
-	let name = '',
+	export let creatingGroup: boolean,
 		groupMembers: any[] = [];
+
+	let name = '';
+
+	const groupChatCreate = async () => {
+		const { res, json } = await fetchRequest(
+			'GET',
+			`user/chat?target_user_ids=${groupMembers.map((member) => member.id)}`
+		);
+
+		if (!res.ok) return;
+
+        creatingGroup = false;
+	};
 </script>
 
-<TextInput autofocus required bind:value={name} label="Chatgroup Name" />
+<form on:submit={groupChatCreate}>
+	<TextInput autofocus required bind:value={name} label="Chatgroup Name" />
 
-Members
-<div>
-	{#each groupMembers as member}
-		{member.name}
-	{/each}
-</div>
+	{$_('Members')}
+	<div>
+		{#each groupMembers as member}
+			{member.name}
+		{/each}
+	</div>
+
+	<Button buttonStyle="primary-light" type="submit">{$_('Confirm')}</Button>
+	<Button buttonStyle="warning-light">{$_('Cancel')}</Button>
+</form>

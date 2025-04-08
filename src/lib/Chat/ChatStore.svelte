82ca9b8abffe28578848ba4240315a4767f17500
Cactu5 <cactusdevcode@gmail.com>
2025-04-08 13:40:37 +0200

    show both chats for testing purposes

--- src/lib/Chat/ChatStore.svelte
+++ src/lib/Chat/ChatStore.svelte
@@ -1,8 +1,11 @@
 <script context="module">
 	import { writable } from 'svelte/store';
 
-	// Store to track if the chat is open (true) or closed (false)
+	// Store to track if the new chat is open
 	export const isChatOpen = writable(false);
+
+	// Store to track if the old chat is open
+	export const isOldChatOpen = writable(false);
 
 	// Store to hold the identifier of the person being talked to.
 	// This can be an object or a simple value as per your application needs.
